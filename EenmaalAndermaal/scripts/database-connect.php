33d859f7c34078fb9d4b7cda3b71962f6b524788
synolog1x <sanderrdtc@gmail.com>
2018-04-23 12:45:02 +0200

    rubriekenboomfuncties toegevoegd

---
+++ EenmaalAndermaal/scripts/database-connect.php
@@ -0,0 +1,12 @@
+<?php
+/*maakt connectie met de database*/
+    $hostname = "mssql.iproject.icasites.nl"; //Naam van de Server
+    $dbname = "iproject1";    //Naam van de Database
+    $username = "iproject1";      //Inlognaam
+    $pw = "JaaK8kbQ8S";      //Password
+
+    $dbh = new PDO ("sqlsrv:Server=$hostname;Database=$dbname;ConnectionPooling=0", "$username", "$pw");
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+?>
---
+++ EenmaalAndermaal/scripts/functions.php
@@ -0,0 +1,31 @@
+<?php
+//rubriekenboom
+function getCategoryOverview($databasehandler)
+{
+    $categoryOverview = "";
+    $mainCategoriesData = $databasehandler->query("SELECT rubrieknummer, rubrieknaam  FROM Rubriek r WHERE Rubriek IS NULL");
+    while ($mainCategoryRow = $mainCategoriesData->fetch()) {
+        //TODO: UIkit element gebruiken ipv section met class
+        $categoryOverview .= '<section class="mainCategorySection"><h2>' . $mainCategoryRow['rubrieknaam'] . '</h2>';
+        $subCategoryData = $databasehandler->query("SELECT rubrieknaam FROM Rubriek r WHERE Rubriek = " . $mainCategoryRow['rubrieknummer']);
+        while ($subCategoryRow = $subCategoryData->fetch()) {
+            //TODO: iets.php veranderen naar werkelijke pagina.
+            $categoryOverview .= '<a href="iets.php?categoryID=' . $subCategoryRow['rubrieknummer'] . '">' . $subCategoryRow['rubrieknaam'] . '</a>';
+        }
+        //TODO: UIkit element gebruiken ipv section met class
+        $categoryOverview .= '</section>';
+    }
+    return $categoryOverview;
+}
+
+function getHomepageCategoryOverview($databasehandler, $overviewSize)
+{
+    $homepageCategoryOverview = "";
+    //TODO: Voorwerp_in_Rubriek aanpassen?
+    $mainCategoriesData = $databasehandler->query("SELECT TOP" . $overviewSize . " rubrieknummer, rubrieknaam  FROM Rubriek r WHERE rubrieknummer IN (SELECT count(*), rubrieknummer FROM Voorwerp_in_Rubriek GROUP BY rubrieknummers ORDER BY count(*)))");
+    while ($mainCategoryRow = $mainCategoriesData->fetch()) {
+        $homepageCategoryOverview .= '<a href="iets.php?categoryID=' . $mainCategoryRow['rubrieknummer'] . '">' . $mainCategoryRow['rubrieknaam'] . '</a>';
+    }
+    return $homepageCategoryOverview;
+}
+?>
