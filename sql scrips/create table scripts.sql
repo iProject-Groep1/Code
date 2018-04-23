1f06ca5ccbba647878f35ad4b2f76b6fbe121379
synolog1x <sanderrdtc@gmail.com>
2018-04-23 16:08:37 +0200

    voorbeeldpopulatie voorwerp, nog niet werkend

--- sql scrips/create table scripts.sql
+++ sql scrips/create table scripts.sql
@@ -1,11 +1,11 @@
 SET DATEFORMAT dmy;
-
+DROP TABLE Voorwerp
 
 CREATE TABLE Voorwerp (
 	Voorwerpnummer			numeric (10)	not null,
 	Title					varchar (30)	not null,						-- aangepast van char 18 naar 
 	beschrijving			varchar (255)	not null,						-- aangepast van char 22
-	startprijs				numeric (6,2)	not null,						-- char 5 
+	startprijs				numeric (8,2)	not null,						-- char 5 
 	betalingswijzen			varchar (10)	not null,						-- char 9
 	betalingsinstructie		varchar (128)			,						-- char 23
 	plaatsnaam				varchar (30)	not null,						-- char 12
@@ -13,7 +13,7 @@ CREATE TABLE Voorwerp (
 	looptijd /*in dagen */	numeric (1)		not null,
 	looptijdbegindag		date			not null,						-- datetime format is aangepast naar dd/mm/yyyy
 	looptijdbeginTijdstip	datetime		not null ,
-	verzendkosten			numeric (4,2)	null ,							-- char 5
+	verzendkosten			numeric (6,2)	null ,							-- char 5
 	verzendinstructies		varchar (128)	null,							-- char 27
 	verkoper				varchar (20)	not null,						-- aangepast van char 10 Ook aanpassen gebruiker table
 	koper					varchar (20)	null,							-- aangepast van char 10 Ook aanpassen gebruiker table
