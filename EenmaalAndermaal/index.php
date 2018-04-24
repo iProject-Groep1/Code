e4f63f07862811463bdd6f79538ec3a89e81a980
TBeeren <tim.beerenn@gmail.com>
2018-04-24 22:15:35 +0200

    Goede versie

--- EenmaalAndermaal/index.php
+++ EenmaalAndermaal/index.php
@@ -1,28 +1,27 @@
 <?php
-require_once ('scripts/header.php');
-include ('scripts/AuctionItem.php');
+require_once('scripts/header.php');
+include('scripts/AuctionItem.php');
 ?>
-
-
 
 
 <body>
 
+<div class="uk-grid uk-align-center">
+    <ul class=" uk-display-inline">
 
-<ul class=" uk-display-inline">
+        <?php
 
-            <?php
+        createItem("iPad Pro");
+        createItem("iPad Pro");
+        createItem("iPad Pro");
+        createItem("iPad Pro");
+        createItem("iPad Pro");
+        createItem("iPad Pro");
 
-            createItem("iPad Pro");
-            createItem("iPad Pro");
-            createItem("iPad Pro");
-            createItem("iPad Pro");
+        ?>
 
-            ?>
-
-</ul>
-
-
+    </ul>
+</div>
 
 
 </body>
--- EenmaalAndermaal/scripts/AuctionItem.php
+++ EenmaalAndermaal/scripts/AuctionItem.php
@@ -3,8 +3,10 @@
 function createItem($productName)
 {
     $echo = '
-<li class="uk-active uk-display-inline"><div class="uk-grid-small uk-child-width-1-4@s uk-flex-center">
-    <div class="uk-card uk-card-default " >
+<li class="uk-active uk-display-inline">
+<div class="uk-grid-small uk-grid-small-changes uk-child-width-1-4@s uk-flex-center uk-display-inline">
+<div class="uk-display-inline-block uk-card uk-card-default">
+    <div class="uk-card uk-card-default ">
     <div class="uk-card-header" >
         <div class="uk-grid-small uk-flex-middle" uk - grid >
             <div class="align-center">
@@ -41,6 +43,8 @@ function createItem($productName)
                 
 </p >
         <a href = "#" class="uk-button tm-button-default uk-button-default2 uk-icon" > BIED NU MEE </a >
+</div>
+</div>
 </div>
 </div>
 </li>';
