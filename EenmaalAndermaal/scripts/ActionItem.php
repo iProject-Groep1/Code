5fa8ca98f36cd9ab3020d4d82975e7cd22143a14
TBeeren <tim.beerenn@gmail.com>
2018-04-24 15:28:18 +0200

    wijzigingen

--- EenmaalAndermaal/scripts/ActionItem.php
+++ EenmaalAndermaal/scripts/ActionItem.php
@@ -8,7 +8,30 @@ function createItem()
         <div class="uk-grid-small uk-flex-middle" uk - grid >
             <div class="uk-width-expand" >
                 <h3 class="uk-card-title uk-margin-remove-bottom" > Title</h3 >
-                <p class="uk-text-meta uk-margin-remove-top" ><time datetime = "2016-04-01T19:00" > April 01, 2016 </time ></p >
+                <p class="uk-text-meta uk-margin-remove-top" >
+                <div class="uk-grid-small uk-child-width-auto" uk-grid uk-countdown="date: 2018-05-01T13:04:01+00:00">
+    <div>
+        <div class="uk-countdown-number uk-countdown-days"></div>
+        <div class="uk-countdown-label uk-margin-small uk-text-center uk-visible@s">Days</div>
+    </div>
+    <div class="uk-countdown-separator">:</div>
+    <div>
+        <div class="uk-countdown-number uk-countdown-hours"></div>
+        <div class="uk-countdown-label uk-margin-small uk-text-center uk-visible@s">Hours</div>
+    </div>
+    <div class="uk-countdown-separator">:</div>
+    <div>
+        <div class="uk-countdown-number uk-countdown-minutes"></div>
+        <div class="uk-countdown-label uk-margin-small uk-text-center uk-visible@s">Minutes</div>
+    </div>
+    <div class="uk-countdown-separator">:</div>
+    <div>
+        <div class="uk-countdown-number uk-countdown-seconds"></div>
+        <div class="uk-countdown-label uk-margin-small uk-text-center uk-visible@s">Seconds</div>
+    </div>
+</div>
+                
+</p >
             </div >
         </div >
     </div >
--- EenmaalAndermaal/scripts/header.php
+++ EenmaalAndermaal/scripts/header.php
@@ -12,136 +12,98 @@
 
 <body>
 
-<nav class="uk-navbar uk-navbar1">
+
+<nav class="uk-navbar-container uk-navbar uk-navbar1" uk-navbar>
     <div class="uk-navbar-left">
-        <a href="#" class="uk-navbar-item uk-logo">
+        <a href="#" class="uk-navbar-item uk-navbar1 uk-logo">
             <img src="images/auction.png" class="uk-margin-small-right" width="32" height="28">EenmaalAndermaal</a>
     </div>
-    <div class="uk-navbar-right">
-        <ul class="uk-navbar-nav uk-visible@m">
-            <li class="uk-active"><a href="#">Home</a></li>
-            <li><a href="#">Rubrieken</a></li>
-            <li><a href="#">Login</a></li>
-            <li><a href="#">Contact</a></li>
+    <div class="uk-navbar-right uk-navbar-blue">
+        <ul class="uk-navbar-nav uk-navbar-nav1 uk-visible@m">
+            <li><a href="">Home</a></li>
+            <li>
+                <a href="#">Rubrieken</a>
+                <div class="uk-navbar-dropdown uk-navbar-dropdown-width-2">
+                    <div class="uk-navbar-dropdown-grid uk-child-width-1-2" uk-grid>
+                        <div>
+                            <ul class="uk-nav uk-navbar-dropdown-nav">
+                                <li class="uk-nav-header">Klassiekers</li>
+                                <li><a href="#">Item</a></li>
+                                <li><a href="#">Item</a></li>
+                                <li class="uk-nav-divider"></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <ul class="uk-nav uk-navbar-dropdown-nav">
+                                <li class="uk-nav-header">Nieuwe types</li>
+                                <li><a href="#">Item</a></li>
+                                <li><a href="#">Item</a></li>
+                                <li class="uk-nav-divider"></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <ul class="uk-nav uk-navbar-dropdown-nav">
+                                <li class="uk-nav-header">Kapotte auto's</li>
+                                <li><a href="#">Item</a></li>
+                                <li><a href="#">Item</a></li>
+                                <li class="uk-nav-divider"></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li><a href="">Login</a></li>
+            <li><a href="">Contact</a></li>
         </ul>
-        <div class="uk-navbar-item uk-visible@m">
+        <div class="uk-navbar-item uk-visible@m uk-navbar1">
             <a href="#" class="uk-button uk-button-default tm-button-default uk-icon">Register now</a>
         </div>
     </div>
 </nav>
 
-<!--<nav class="uk-navbar uk-navbar1">-->
-<!---->
-<!--    <a class="uk-navbar-brand uk-hidden-small" href="">EenmaalAndermaal</a>-->
-<!---->
-<!--    <ul class="uk-navbar-nav uk-navbar-nav1 uk-hidden-small uk-grid-divider ">-->
-<!---->
-<!--        <li class="uk-parent" data-uk-dropdown="">-->
-<!--            <a href="">Home</a>-->
-<!--        </li>-->
-<!---->
-<!--        <li class="uk-parent" data-uk-dropdown="">-->
-<!--            <a href="">Rubrieken</a>-->
-<!---->
-<!--            <div class="uk-dropdown uk-dropdown-navbar">-->
-<!--                <ul class="uk-nav uk-nav-navbar">-->
-<!--                    <li class="uk-nav-header">Alle rubrieken</li>-->
-<!--                    <li><a href="#">Item</a></li>-->
-<!--                    <li><a href="#">Item</a></li>-->
-<!--                    <li class="uk-nav-divider"></li>-->
-<!--                    <li><a href="#">Separated item</a></li>-->
-<!--                    <li class="uk-parent">-->
-<!--                        <a href="#">Parent</a>-->
-<!--                        <ul class="uk-nav-sub">-->
-<!--                            <li><a href="#">Sub item</a>-->
-<!--                                <ul>-->
-<!--                                    <li><a href="#">Sub item</a></li>-->
-<!--                                </ul>-->
-<!--                            </li>-->
-<!--                        </ul>-->
-<!--                    </li>-->
-<!--                </ul>-->
-<!--            </div>-->
-<!---->
-<!---->
-<!---->
-<!--        </li>-->
-<!---->
-<!--        <li class="uk-parent" data-uk-dropdown="" aria-haspopup="true" aria-expanded="false">-->
-<!--            <a href="">Login</a>-->
-<!--        </li>-->
-<!---->
-<!--        <li class="uk-parent" data-uk-dropdown="" aria-haspopup="true" aria-expanded="false">-->
-<!--            <a href="">Contact</a>-->
-<!--            <div class="uk-dropdown uk-dropdown-navbar uk-dropdown-bottom" aria-hidden="true" tabindex="" style="top: 40px; left: 0px;">-->
-<!--                <div class="uk-panel">Lorem ipsum dolor sit amet, consectetur <a href="#">adipisicing</a> elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>-->
-<!--            </div>-->
-<!--        </li>-->
-<!---->
-<!--    </ul>-->
-<!---->
-<!--    <a href="#" class="uk-navbar-toggle uk-visible-small" data-uk-offcanvas="{target:'#offcanvas-1'}"></a>-->
-<!---->
-<!--</nav>-->
-<!---->
-<!--<nav class="uk-navbar uk-navbar2">-->
-<!---->
-<!--    <ul class="uk-navbar-nav uk-navbar-nav2 uk-hidden-small ">-->
-<!---->
-<!--        <li>-->
-<!--            <a href="">Alle Rubrieken</a>-->
-<!---->
-<!--        </li><li>-->
-<!--            <a href="">Huis en tuin</a>-->
-<!--        </li>-->
-<!---->
-<!--        <li class="uk-parent" data-uk-dropdown="">-->
-<!--            <a href="">Auto's</a>-->
-<!---->
-<!--            <div class="uk-dropdown uk-dropdown-navbar">-->
-<!--                <ul class="uk-nav uk-nav-navbar">-->
-<!--                    <li class="uk-nav-header">Klassiekers</li>-->
-<!--                    <li><a href="#">Item</a></li>-->
-<!--                    <li><a href="#">Item</a></li>-->
-<!--                    <li class="uk-nav-header">Nieuwe modellen</li>-->
-<!--                    <li><a href="#">Item</a></li>-->
-<!--                    <li><a href="#">Item</a></li>-->
-<!--                    <li class="uk-nav-header">Kapotte auto's</li>-->
-<!--                    <li><a href="#">Item</a></li>-->
-<!--                    <li><a href="#">Item</a></li>-->
-<!--                </ul>-->
-<!--            </div>-->
-<!---->
-<!---->
-<!---->
-<!--        </li>-->
-<!---->
-<!--        <li>-->
-<!--            <a href="">Elektronica</a>-->
-<!--        </li>-->
-<!---->
-<!--        <li>-->
-<!--            <a href="">Boeken</a>-->
-<!--        </li>-->
-<!---->
-<!--        <li>-->
-<!--            <a href="">Meubels</a>-->
-<!--        </li>-->
-<!---->
-<!--        <li>-->
-<!--            <a href="">Brommers</a>-->
-<!--        </li>-->
-<!---->
-<!--        <li>-->
-<!--            <a href="">Huisdieren</a>-->
-<!--        </li>-->
-<!---->
-<!--        <li>-->
-<!--            <a href="">Vrije tijd</a>-->
-<!--        </li>-->
-<!---->
-<!--    </ul>-->
-<!---->
-<!--    <a href="#" class="uk-navbar-toggle uk-visible-small" data-uk-offcanvas="{target:'#offcanvas-1'}"></a>-->
-<!---->
-<!--</nav>-->
+
+<nav class="uk-navbar-container uk-navbar uk-navbar2" uk-navbar>
+    <div class="uk-navbar-center">
+        <ul class="uk-navbar-nav uk-navbar-nav2 uk-visible@m">
+            <li><a href="">Alle Rubrieken</a></li>
+            <li><a href="">Huis en tuin</a></li>
+            <li>
+                <a href="#">Auto's</a>
+                <div class="uk-navbar-dropdown uk-navbar-dropdown-width-2">
+                    <div class="uk-navbar-dropdown-grid uk-child-width-1-2" uk-grid>
+                        <div>
+                            <ul class="uk-nav uk-navbar-dropdown-nav">
+                                <li class="uk-nav-header">Klassiekers</li>
+                                <li><a href="#">Item</a></li>
+                                <li><a href="#">Item</a></li>
+                                <li class="uk-nav-divider"></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <ul class="uk-nav uk-navbar-dropdown-nav">
+                                <li class="uk-nav-header">Nieuwe types</li>
+                                <li><a href="#">Item</a></li>
+                                <li><a href="#">Item</a></li>
+                                <li class="uk-nav-divider"></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <ul class="uk-nav uk-navbar-dropdown-nav">
+                                <li class="uk-nav-header">Kapotte auto's</li>
+                                <li><a href="#">Item</a></li>
+                                <li><a href="#">Item</a></li>
+                                <li class="uk-nav-divider"></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li><a href="">Elektronica</a></li>
+            <li><a href="">Boeken</a></li>
+            <li><a href="">Meubels</a></li>
+            <li><a href="">Brommers</a></li>
+            <li><a href="">Huisdieren</a></li>
+            <li><a href="">Vrije tijd</a></li>
+        </ul>
+    </div>
+</nav>
