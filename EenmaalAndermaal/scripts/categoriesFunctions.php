8a490d1449c3b50978aa053407bc47f8006429dd
synolog1x <sanderrdtc@gmail.com>
2018-04-24 11:40:42 +0200

    rubriekenboomfuncties verder uitgewerkt.

--- EenmaalAndermaal/scripts/categoriesFunctions.php
+++ EenmaalAndermaal/scripts/categoriesFunctions.php
@@ -2,19 +2,38 @@
 //rubriekenboom
 function getCategoryOverview($databasehandler)
 {
+    $previousCategoryNumber = 0;
     $alphabet = range('A', 'Z');
     $categoryOverview = "";
-    $mainCategoriesData = $databasehandler->query("SELECT rubrieknummer, rubrieknaam  FROM Rubriek r WHERE Rubriek IS NULL");
-    while ($mainCategoryRow = $mainCategoriesData->fetch()) {
-        //TODO: UIkit element gebruiken ipv section met class
-        $categoryOverview .= '<section class="mainCategorySection"><h2>' . $mainCategoryRow['rubrieknaam'] . '</h2>';
-        $subCategoryData = $databasehandler->query("SELECT rubrieknaam FROM Rubriek r WHERE Rubriek = " . $mainCategoryRow['rubrieknummer']);
-        while ($subCategoryRow = $subCategoryData->fetch()) {
-            //TODO: iets.php veranderen naar werkelijke pagina.
-            $categoryOverview .= '<a href="iets.php?categoryID=' . $subCategoryRow['rubrieknummer'] . '">' . $subCategoryRow['rubrieknaam'] . '</a>';
+    foreach ($alphabet as $letter) {
+        $categoryOverview .= '<section class="mainCategorySection"><h1>' . $letter . '</h1>';
+        $mainCategoriesData = $databasehandler->query("SELECT rubrieknummer, rubrieknaam  FROM Rubriek r WHERE volgnummer = rubrieknummer");
+        while ($mainCategoryRow = $mainCategoriesData->fetch()) {
+            //TODO: UIkit element gebruiken ipv section met class
+            $categoryOverview .= '<h2>' . $mainCategoryRow['rubrieknaam'] . '</h2>';
+            $previousCategoryNumber = $mainCategoryRow['rubrieknummer'];
+
+            if ($count = $databasehandler->query("SELECT COUNT(*) FROM Rubriek r WHERE Rubriek = " . $previousCategoryNumber)) {
+                if ($count->fetchColumn() > 0) {
+                    $subCategoryData = $databasehandler->query("SELECT rubrieknummer, rubrieknaam FROM Rubriek r WHERE Rubriek = " . $previousCategoryNumber);
+                    while ($subCategoryRow = $subCategoryData->fetch()) {
+                        //TODO: iets.php veranderen naar werkelijke pagina.
+                        $categoryOverview .= '<a href="iets.php?categoryID=' . $subCategoryRow['rubrieknummer'] . '">' . $subCategoryRow['rubrieknaam'] . '</a>';
+                        $previousCategoryNumber = $subCategoryRow['rubrieknummer'];
+                        if ($count2 = $databasehandler->query("SELECT COUNTR(*) FROM Rubriek r WHERE Rubriek =" . $previousCategoryNumber)) {
+                            if ($count2->fetchColumn() > 0) {
+                                $subCategoryData2 = $databasehandler->query("SELECT rubrieknummer, rubrieknaam FROM Rubriek r WHERE Rubriek = " . $previousCategoryNumber);
+                                while ($subCategoryRow2 = $subCategoryData2->fetch()) {
+                                    $categoryOverview .= '<a href="iets.php?categoryID=' . $subCategoryRow2['rubrieknummer'] . '">' . $subCategoryRow2['rubrieknaam'] . '</a>';
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            //TODO: UIkit element gebruiken ipv section met class
+            $categoryOverview .= '</section>';
         }
-        //TODO: UIkit element gebruiken ipv section met class
-        $categoryOverview .= '</section>';
     }
     return $categoryOverview;
 }
@@ -30,15 +49,17 @@ function getHomepageCategoryOverview($databasehandler, $overviewSize)
     return $homepageCategoryOverview;
 }
 
-function getAlphabetList(){
+function getAlphabetList()
+{
     $alphabet = range('A', 'Z');
     $alphabetList = '<ul class="uk-pagination">';
-    foreach($alphabet as $letter){
-        $alphabetList .= '<li><a href="">'.$letter.'</a></li>';
+    foreach ($alphabet as $letter) {
+        $alphabetList .= '<li><a href="">' . $letter . '</a></li>';
     }
     $alphabetList .= '</ul>';
     return $alphabetList;
 }
+
 ?>
 
 
