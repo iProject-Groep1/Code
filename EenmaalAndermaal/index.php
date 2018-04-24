0a1dc882cddd70cfe268d8bc8eff46201464fa7b
JasmijnB97 <jasmijn.bartelds@hotmail.nl>
2018-04-24 11:16:09 +0200

    afbeeldingen eerste rij

--- EenmaalAndermaal/index.php
+++ EenmaalAndermaal/index.php
@@ -13,7 +13,7 @@
 
 <body>
 
-<nav class="uk-navbar ">
+<nav class="uk-navbar uk-navbar1">
 
     <a class="uk-navbar-brand uk-hidden-small" href="">EenmaalAndermaal</a>
 
@@ -134,20 +134,17 @@
     <div class="uk-container">
         <h3>Laatste kans</h3>
 
-        <a class="uk-thumbnail uk-overlay-hover" href="#">
-            <figure class="uk-overlay">
-                <img src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMzAwIiBoZWlnaHQ9IjExMCIgdmlld0JveD0iMCAwIDMwMCAxMTAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzMwMHgxMTAvYXV0bwpDcmVhdGVkIHdpdGggSG9sZGVyLmpzIDIuNS4yLgpMZWFybiBtb3JlIGF0IGh0dHA6Ly9ob2xkZXJqcy5jb20KKGMpIDIwMTItMjAxNSBJdmFuIE1hbG9waW5za3kgLSBodHRwOi8vaW1za3kuY28KLS0+PGRlZnMvPjxyZWN0IHdpZHRoPSIzMDAiIGhlaWdodD0iMTEwIiBmaWxsPSIjRUVFRUVFIi8+PGc+PHRleHQgeD0iMTExLjQwNjI1IiB5PSI2MS42NjU2MjUwMDAwMDAwMDYiIHN0eWxlPSJmaWxsOiNBQUFBQUE7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6MTVwdCI+MzAweDExMDwvdGV4dD48L2c+PC9zdmc+" alt="300x110" data-src="holder.js/300x110/auto" data-holder-rendered="true">
-                <div class="uk-overlay-panel uk-overlay-background uk-overlay-bottom uk-overlay-slide-bottom">Bieden</div>
-            </figure>
-            <div class="uk-thumbnail-caption">item</div>
-        </a>
-        <a class="uk-thumbnail uk-overlay-hover" href="#">
-            <figure class="uk-overlay">
-                <img src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMzAwIiBoZWlnaHQ9IjExMCIgdmlld0JveD0iMCAwIDMwMCAxMTAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzMwMHgxMTAvYXV0bwpDcmVhdGVkIHdpdGggSG9sZGVyLmpzIDIuNS4yLgpMZWFybiBtb3JlIGF0IGh0dHA6Ly9ob2xkZXJqcy5jb20KKGMpIDIwMTItMjAxNSBJdmFuIE1hbG9waW5za3kgLSBodHRwOi8vaW1za3kuY28KLS0+PGRlZnMvPjxyZWN0IHdpZHRoPSIzMDAiIGhlaWdodD0iMTEwIiBmaWxsPSIjRUVFRUVFIi8+PGc+PHRleHQgeD0iMTExLjQwNjI1IiB5PSI2MS42NjU2MjUwMDAwMDAwMDYiIHN0eWxlPSJmaWxsOiNBQUFBQUE7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6MTVwdCI+MzAweDExMDwvdGV4dD48L2c+PC9zdmc+" alt="300x110" data-src="holder.js/300x110/auto" data-holder-rendered="true">
-                <div class="uk-overlay-panel uk-overlay-background uk-overlay-bottom uk-overlay-slide-bottom">Bieden</div>
-            </figure>
-            <div class="uk-thumbnail-caption">item</div>
-        </a>
+        <ul class="uk-grid uk-grid-width-1-4">
+            <?php for($i = 0; $i <= 3; $i++){ ?>
+                <a class="uk-thumbnail uk-overlay-hover" href="#">
+                    <li class="uk-overlay">
+                        <img src="images/bank.jpg" alt=""/>
+                        <div class="uk-overlay-panel uk-overlay-background uk-overlay-bottom uk-overlay-slide-bottom">Bieden</div>
+                    </li>
+                    <div class="uk-thumbnail-caption">item</div>
+                </a>
+            <?php } ?>
+        </ul>
 
     </div>
 </div>
@@ -176,6 +173,8 @@
         </a>
 
 </div>
+
+
 
 
 </body>
