9019d31fe6c513c9ddf3d08ca39533295f986026
TBeeren <tim.beerenn@gmail.com>
2018-04-24 18:04:58 +0200

    Mooie veilingen met variabels

--- EenmaalAndermaal/scripts/AuctionItem.php
+++ EenmaalAndermaal/scripts/AuctionItem.php
@@ -1,14 +1,22 @@
 <?php
 
-function createItem()
+function createItem($productName)
 {
     $echo = '
+<li class="uk-active uk-display-inline"><div class="uk-grid-small uk-child-width-1-4@s uk-flex-center">
     <div class="uk-card uk-card-default " >
     <div class="uk-card-header" >
         <div class="uk-grid-small uk-flex-middle" uk - grid >
-            <div class="uk-width-expand" >
-                <h3> Product 1 </h3>
-                <p class="uk-text-meta uk-margin-remove-top" >
+            <div class="align-center">
+                <h3>' . $productName . '</h3>
+            </div >
+        </div >
+    </div >
+    <div class="uk-card-body" >
+    <img src="images/ipad.jpg" class="uk-align-center" width="300" height="200">
+    </div >
+    <div class="uk-card-footer" >
+    <p class="uk-text-meta uk-margin-remove-top" >
                 <div class="uk-grid-small uk-child-width-auto" uk-grid uk-countdown="date: 2018-05-01T13:04:01+00:00">
     <div>
         <div class="uk-countdown-number uk-countdown-days"></div>
@@ -32,16 +40,10 @@ function createItem()
 </div>
                 
 </p >
-            </div >
-        </div >
-    </div >
-    <div class="uk-card-body" >
-        <p > Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt .</p >
-    </div >
-    <div class="uk-card-footer" >
-        <a href = "#" class="uk-button uk-button-text" > BIED NU MEE </a >
-    </div >
-</div >';
+        <a href = "#" class="uk-button tm-button-default uk-button-default2 uk-icon" > BIED NU MEE </a >
+</div>
+</div>
+</li>';
 
     echo $echo;
 }
