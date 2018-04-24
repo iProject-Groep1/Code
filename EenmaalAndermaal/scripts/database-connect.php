409fc01f0418647f1eb3b59988c4d90685fc09df
synolog1x <sanderrdtc@gmail.com>
2018-04-24 09:52:04 +0200

    commit

--- EenmaalAndermaal/scripts/database-connect.php
+++ EenmaalAndermaal/scripts/database-connect.php
@@ -3,7 +3,7 @@
     $hostname = "mssql.iproject.icasites.nl"; //Naam van de Server
     $dbname = "iproject1";    //Naam van de Database
     $username = "iproject1";      //Inlognaam
-    $pw = "JaaK8kbQ8S";      //Password
+    $pw = "JaaK8kbQ8S";      //Wachtwoord
 
     $dbh = new PDO ("sqlsrv:Server=$hostname;Database=$dbname;ConnectionPooling=0", "$username", "$pw");
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
