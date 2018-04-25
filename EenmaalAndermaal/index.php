50c686cec9b1c2f3365c8fc3bc61e4692bff5426
TBeeren <tim.beerenn@gmail.com>
2018-04-25 11:31:56 +0200

    Css fixed

--- EenmaalAndermaal/index.php
+++ EenmaalAndermaal/index.php
@@ -12,8 +12,8 @@ include('scripts/AuctionItem.php');
 
         createItem("iPad Pro", "2018-04-25T10:00:00+00:00");
         createItem("iPad Pro","2018-04-25T11:00:00+00:00");
-//        createItem("iPad Pro", 3);
-//        createItem("iPad Pro", 4);
+        createItem("iPad Pro", "2018-04-25T12:00:00+00:00");
+        createItem("iPad Pro", "2018-04-25T13:00:00+00:00");
 //        createItem("iPad Pro", 5);
 //        createItem("iPad Pro", 6);
 
--- EenmaalAndermaal/scripts/HomepageFunctions.php
+++ EenmaalAndermaal/scripts/HomepageFunctions.php
@@ -1,34 +1,15 @@
-function dbConnect {
-  try {
-    $hostname = "mssql.iproject.icasites.nl"; //Naam van de Server
-    $dbname = "iproject1";    //Naam van de Database
-    $username = "iproject1";      //Inlognaam
-    $pw = "JaaK8kbQ8S";      //Password
-
-    $pdo = new PDO ("sqlsrv:Server=$hostname;Database=$dbname;ConnectionPooling=0","$username","$pw");
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    return $pdo; // "Connectie kunnen maken met de database!";
-    } catch (PDOException $e) {
-        echo "Fout" . $e->getMessage();
-    }
-
-}
-
-
-
-
+<?php
 
 
 function getTimeToEnd {
-  try {
-    $stmt = dbConnect()->prepare("
+    try {
+        $stmt = dbConnect()->prepare("
  	 select v.looptijdeindeDag, v.looptijdeindeTijdstip, v.veilinggesloten 
- 	 from Voorwerp v 
+ 	 from Voorwerp v ");
 
-	 TODO: het stmt is nog niet compleet qua tijden
+	/*  TODO: het stmt is nog niet compleet qua tijden */
 
-    $stmt->execute(); 
+    $stmt->execute();
     $filmData = $stmt->fetch(PDO::FETCH_ASSOC);
 
     return $filmData;
@@ -43,21 +24,3 @@ function getTimeToEnd {
 
 
 
-
-/* In de parameter kan je nu opgeven tot hoeveel minuten deze filter ingesteld moet worden. */
-
-function setEndingAuctions ($minutesToEnding){
-
-  $itemTime = …  //TODO , een functie maken die de tijd van nu vergelijkt met de tijd van de veiling
-  $EndingAuctionsList = {}; // TODO, Moet buiten de functie staan
-  $numberOfAuctions = 20;
-
-
-    if ($itemTime < $minutesToEnding) {
-      if ($index < $numberOfAuctions ){
-  	EndingAuctionsList[$index] = $itemTime // Dit moet een ID of titel etc zijn het voorwerp zijn.
-    }
-    $index ++;
-  }
-
-}
