8ee31f7edbc8ac7716c50e41d82033cff3c71e4a
synolog1x <sanderrdtc@gmail.com>
2018-05-07 13:33:21 +0200

    categoryOverviewFunctions aangepast

--- EenmaalAndermaal/categoryOverview.php
+++ EenmaalAndermaal/categoryOverview.php
@@ -1,9 +1,9 @@
 <?php
 require_once('scripts/database-connect.php');
 require_once('scripts/header.php');
-require_once('scripts/categoriesFunctions.php');
+require_once('scripts/categoryOverviewFunctions.php');
 
-echo getAlphabetList();
+//echo getAlphabetList();
 echo getCategoryOverview($dbh);
 ?>
 
