cb6be1310a25ab92047d44de4bcb067038db4447
TBeeren <tim.beerenn@gmail.com>
2018-05-14 13:05:37 +0200

    bugs

--- EenmaalAndermaal/verification.php
+++ EenmaalAndermaal/verification.php
@@ -11,8 +11,8 @@ activateAccount($dbh);
 
 function activateAccount($dbh)
 {
-$match = 0;
-    if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])){
+    $match = 0;
+    if (isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])) {
         // Verify data
         $email = $_GET['email']; // Set email variable
         $hash = $_GET['hash']; // Set hash variable
@@ -21,66 +21,71 @@ $match = 0;
     $search = $dbh->query("SELECT email, hash FROM Verificatie WHERE email='" . $email . "' AND hash='" . $hash . "'");
 
 
-echo $match;
-
+    echo $match;
+}
 
 ?>
+
 <body>
-  <form>
+<form>
     <div class="uk-margin">
-            <input class="uk-input" type="text" placeholder="<? $hach ?>">
-        </div>
-        <div class="uk-margin">
-            <input class="uk-input" type="text" placeholder="<? $email ?>">
-        </div>
-        <div class="uk-margin">
-            <input class="uk-input" type="text" placeholder="gebruikersnaam">
-        </div>
-        <div class="uk-margin">
-            <input class="uk-input" type="text" placeholder="voornaam">
-        </div>
-        <div class="uk-margin">
-            <input class="uk-input" type="text" placeholder="achternaam">
-        </div>
-        <div class="uk-margin">
-            <input class="uk-input" type="text" placeholder="Eerste adres">
-        </div>
-        <div class="uk-margin">
-            <input class="uk-input" type="text" placeholder="Twede adres">
-        </div>
-        <div class="uk-margin">
-            <input class="uk-input" type="text" placeholder="Postcode">
-        </div>
-        <div class="uk-margin">
-            <input class="uk-input" type="text" placeholder="Plaatsnaam">
-        </div>
-        <div class="uk-margin">
-           <select class="uk-select">
-               <?PHP Get_country($dbh) ?>
-           </select>
-       </div>
-        <div class="uk-margin">
-            <input class="uk-input" type="text" placeholder="Input">
-        </div>
-        <div class="uk-margin">
-            <input class="uk-input" type="text" placeholder="Input">
-        </div>
-        <div class="uk-margin">
-            <input class="uk-input" type="text" placeholder="Input">
-        </div>
-        <div class="uk-margin">
-            <input class="uk-input" type="text" placeholder="Input">
-        </div>
-        <div class="uk-margin">
-            <input class="uk-input" type="text" placeholder="Input">
-        </div>
-        <div class="uk-margin">
-            <input class="uk-input" type="text" placeholder="Input">
-        </div>
-        <div class="uk-margin">
-            <input class="uk-input" type="text" placeholder="Input">
-        </div>
+        <input class="uk-input" type="text" placeholder="<? $hash ?>">
+    </div>
+    <div class="uk-margin">
+        <input class="uk-input" type="text" placeholder="<? $email ?>">
+    </div>
+    <div class="uk-margin">
+        <input class="uk-input" type="text" placeholder="gebruikersnaam">
+    </div>
+    <div class="uk-margin">
+        <input class="uk-input" type="text" placeholder="voornaam">
+    </div>
+    <div class="uk-margin">
+        <input class="uk-input" type="text" placeholder="achternaam">
+    </div>
+    <div class="uk-margin">
+        <input class="uk-input" type="text" placeholder="Eerste adres">
+    </div>
+    <div class="uk-margin">
+        <input class="uk-input" type="text" placeholder="Twede adres">
+    </div>
+    <div class="uk-margin">
+        <input class="uk-input" type="text" placeholder="Postcode">
+    </div>
+    <div class="uk-margin">
+        <input class="uk-input" type="text" placeholder="Plaatsnaam">
+    </div>
+    <div class="uk-margin">
+        <select class="uk-select">
+            <?PHP Get_country($dbh) ?>
+        </select>
+    </div>
+    <div class="uk-margin">
+        <input class="uk-input" type="text" placeholder="Input">
+    </div>
+    <div class="uk-margin">
+        <input class="uk-input" type="text" placeholder="Input">
+    </div>
+    <div class="uk-margin">
+        <input class="uk-input" type="text" placeholder="Input">
+    </div>
+    <div class="uk-margin">
+        <input class="uk-input" type="text" placeholder="Input">
+    </div>
+    <div class="uk-margin">
+        <input class="uk-input" type="text" placeholder="Input">
+    </div>
+    <div class="uk-margin">
+        <input class="uk-input" type="text" placeholder="Input">
+    </div>
+    <div class="uk-margin">
+        <input class="uk-input" type="text" placeholder="Input">
+    </div>
 
 
-  </form>
+</form>
 </body>
+
+
+
+
