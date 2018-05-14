480051be2dc5af12ef18c1ae7c3db97c9a69e189
TBeeren <tim.beerenn@gmail.com>
2018-05-14 12:49:32 +0200

    done

--- EenmaalAndermaal/verification.php
+++ EenmaalAndermaal/verification.php
@@ -1,19 +1,28 @@
 <?php
+include('scripts/database-connect.php');
 
-if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])){
-    // Verify data
-    $email = $_GET['email']; // Set email variable
-    $hash = $_GET['hash']; // Set hash variable
+activateAccount($dbh);
+
+function activateAccount($dbh)
+{
+$match = 0;
+    if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])){
+        // Verify data
+        $email = $_GET['email']; // Set email variable
+        $hash = $_GET['hash']; // Set hash variable
+    }
+
+    $search = $dbh->query("SELECT email, hash FROM Verificatie WHERE email='" . $email . "' AND hash='" . $hash . "'");
+
+    while ($row = $search->fetch()) {
+        $match ++;
+    }
+
+    if ($match > 0) {
+        header('index.php');
+        echo "Account geactiveerd";
+
+    } else {
+        echo "invalid url or account has already been activated.";
+    }
 }
-
-$search = ("SELECT email, hash FROM Verificatie WHERE email='".$email."' AND hash='".$hash."'");
-$match  = mssql_num_rows($search);
-
-if($match > 0){
-    echo "Account geactiveerd";
-}else{
-    echo "invalid url or account has already been activated.";
-}
-
-
-echo $match;
