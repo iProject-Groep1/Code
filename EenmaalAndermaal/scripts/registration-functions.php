9cd8679e9f606429c063282edc2428f2d739598f
synolog1x <sanderrdtc@gmail.com>
2018-05-15 13:19:12 +0200

    email opgeleukt

--- EenmaalAndermaal/scripts/registration-functions.php
+++ EenmaalAndermaal/scripts/registration-functions.php
@@ -6,7 +6,7 @@ include('database-connect.php');
 /* ALLE INLOG PAGINA FUNCTIES */
 /* Deze 2 if statements zorgen ervoor dat er gecheckt wordt of er gesubmit is.
  Indien een van de 2 forms ingevuld is start hij een functie. */
-if(isset($_POST["submit"])) {
+if (isset($_POST["submit"])) {
     emailReg($dbh);
 }
 
@@ -41,19 +41,31 @@ function emailReg($dbh)
 function createMessage($email, $hash)
 {
     $to = $email; // Send email to our user
-    $subject = 'Signup | Verification'; // Give the email a subject
+    $subject = 'Registratie | Verificatie EenmaalAndermaal'; // Give the email a subject
     $message = '
- 
-Thanks for signing up!
-Your account has been created, you can login after you have activated your account by pressing the url below.
- 
-------------------------
-Email: ' . $email . '
-Verification Code: ' . $hash . '
-------------------------
- 
-Please click this link to activate your account:
-http://iproject1.icasites.nl/verification.php?email='.$email.'&hash='.$hash.''; // Our message above including the link
+ <!DOCTYPE HTML>
+ <html>
+ <head></head>
+<body>
+<h1>Bedankt voor het registreren!</h1>
+<div>
+<p>Je account is gemaakt, nadat je gegevens hebt ingevuld kan je inloggen op <a href="http://iproject1.icasites.nl/login.php">EenmaalAndermaal</a></p>
+<p>Klik op <a href="http://iproject1.icasites.nl/verification.php?email=' . $email . '&hash=' . $hash . '">deze link</a> om je gegevens in te vullen.</p>
+ </div>
+
+<div> 
+<p>Uw account is gemaakt met het volgende e-mail adres:</p>
+<p>' . $email . '</p>
+</div>
+<div>
+<p>Uw verificatie code is:</p>
+<p>' . $hash . '</p>
+</div>
+
+
+</body>
+</html>
+'; // Our message above including the link
 
 
     $headers = 'From:noreply@EenmaalAndermaal.com' . "\r\n"; // Set from headers
@@ -61,11 +73,11 @@ http://iproject1.icasites.nl/verification.php?email='.$email.'&hash='.$hash.''; 
 }
 
 
-
-function verifyEmail($hash, $dbh){
+function verifyEmail($hash, $dbh)
+{
 
     try {
-        $sql = $dbh() ->prepare("SELECT hash FROM users WHERE hash = :hash");
+        $sql = $dbh()->prepare("SELECT hash FROM users WHERE hash = :hash");
         if ($sql == false) {
             echo 'Failed to prepare statement';
         }
@@ -102,7 +114,7 @@ function verifyEmail($hash, $dbh){
 function register($dbh)
 {
 
-    if ($_POST['wachtwoord'] != $_POST['wachtwoord herhaal']){
+    if ($_POST['wachtwoord'] != $_POST['wachtwoord herhaal']) {
         echo 'Wachtwoord komt niet overeen';
         return;
     }
@@ -118,7 +130,6 @@ function register($dbh)
     $email = ($_POST['email']);
 
 
-
     try {
 
 
@@ -131,18 +142,12 @@ function register($dbh)
 
         }
 
-        sanitizing_input($firstname, $lastname, $username,  $email);
+        sanitizing_input($firstname, $lastname, $username, $email);
 
 
         $sql = "insert into Users (firstname, lastname, country, birth, username, password, email) values (?, ?, ?, ?, ?, ?, ?)";
         $query = dbconnect()->prepare($sql);
         $query->execute(array($firstname, $lastname, $country, $birth, $username, $passwordhash, $email));
-
-
-
-
-
-
 
 
     } catch (PDOException $e) {
@@ -151,9 +156,6 @@ function register($dbh)
     header("Location: login.php");
     $_SESSION['messages'][] = "Bedankt voor uw registratie " . $firstname . "!";
 }
-
-
-
 
 
 ?>
