23e2513b19f3f1593e50d18db1192b6a3f5b1194
synolog1x <sanderrdtc@gmail.com>
2018-05-17 14:42:10 +0200

    groot deel van verificatie gefixt

--- EenmaalAndermaal/scripts/newUser.php
+++ EenmaalAndermaal/scripts/newUser.php
@@ -1,124 +1,163 @@
 <?php
 include('database-connect.php');
 
-if (isset($_POST['submit'])){
-  registerUser($dbh);
+if (isset($_POST['submit'])) {
+    registerUser($dbh);
 }
 
 function registerUser($dbh)
 {
+    $dataCorrect = false;
+    $passwordCorrect = false;
+    $usernameCorrect = false;
+    $emailCorrect = false;
+    $emailUniek = false;
 
+    $firstname = $_POST['Voornaam'];
+    $lastname = $_POST['Achternaam'];
+    $firstAddress = $_POST['EersteAdres'];
+    $secondAddress = $_POST['TweedeAdres'];
+    $postcode = $_POST['Postcode'];
+    $plaatsnaam = $_POST['Plaatsnaam'];
+    $country = $_POST['Land'];
+    $birthDate = $_POST['Datum'];
+    $username = $_POST['Gebruikersnaam'];
+    $password = "";
+    $passwordHash = "";
+
+    $securityQuestion = $_POST['vraag'];
+    $securityQuestionAnswer = $_POST['Antwoord'];
+    $email = $_POST['email'];
+
+    //controleer of wachtwoorden overeen komen
     if ($_POST['Wachtwoord'] != $_POST['Wachtwoord_bevestigen']) {
-        echo 'Wachtwoord komt niet overeen';
-        header('Location:newUser.php');
+        $passwordCorrect = false;
     } else {
-
-        $vraag = 0;
-
-        //Alle variabelen van de Form
-        $firstname = $_POST['Voornaam'];
-        $lastname = $_POST['Achternaam'];
-        $EersteAdres = $_POST['EersteAdres'];
-        $TweedeAdres = $_POST['TweedeAdres'];
-        $Postcode = $_POST['Postcode'];
-        $Plaatsnaam = $_POST['Plaatsnaam'];
-        $country = $_POST['Land'];
-        $birth = $_POST['Datum'];
-        $username = $_POST['Gebruikersnaam'];
-        $password = $_POST['Wachtwoord'];
+        $passwordCorrect = true;
+        $password = $password = $_POST['Wachtwoord'];
         $passwordhash = password_hash($password, PASSWORD_DEFAULT);
-        $vraag = $_POST['vraag'];
-        $antwoord = $_POST['Antwoord'];
-        if (isset($_GET['email']) && !empty($_GET['email'])) {
-            $email = ($_GET['email']);
-        };
-
-
-        try {
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
-                echo("$email is a valid email address");
-            } else {
-                /*    header("Location: login.php");*/
-                $_SESSION['messages'][] = 'Dit email adress is niet correct. Vul een valide email is a.u.b .';
-                exit ('Velden zijn hetzelfde');
-            }
-
-
-            /*  sanitizing_input($firstname, $lastname, $username,  $email);*/
-            sanitizing_input($username, $firstname, $lastname, $EersteAdres, $TweedeAdres, $Postcode, $Plaatsnaam, $antwoord, $email, $dbh);
-
-            $sql = "insert into Gebruiker ([gebruikersnaam], [voornaam], [achternaam], [adresregel1], [adresregel2], [postcode], [plaatsnaam], [land], [geboortedag], [mail_adres], [wachtwoord], [vraag], [antwoordtekst])
-        values (?, ?, ?, ?, ?, ?, ?,?,?,?,?,?,?)";
-            $query = $dbh->prepare($sql);
-            $query->execute(array($username, $firstname, $lastname, $EersteAdres, $TweedeAdres, $Postcode, $Plaatsnaam, $country, $birth, $email, $passwordhash, $vraag, $antwoord));
-
-
-            $_SESSION['regMelding'] = '
-    <script>UIkit.notification({message: \'Bedankt voor de registratie ' . $username . '!\', status: \'danger\'})</script>
-    ';
-
-
-        } catch (PDOException $e) {
-            echo "Fout" . $e->getMessage();
-        }
-
-
-
-    $_SESSION['messages'][] = "Bedankt voor uw registratie " . $firstname . "!";
-
-    
-        $_SESSION['messages'][] = "Bedankt voor uw registratie " . $firstname . "!";
-        header("Location: ../login.php");
-
     }
-}
-
-function sanitizing_input($username, $firstname, $lastname, $EersteAdres, $TweedeAdres, $Postcode, $Plaatsnaam, $antwoord,$email, $dbh)
-{
-    trim($firstname);
-    trim($lastname);
-    ucfirst($firstname);
-    ucfirst($lastname);
-    htmlspecialchars($firstname);
-    htmlspecialchars($lastname);
-    htmlspecialchars($EersteAdres);
-    htmlspecialchars($TweedeAdres);
-    htmlspecialchars($Postcode);
-    htmlspecialchars($Plaatsnaam);
-    htmlspecialchars($antwoord);
 
     try {
-
-        $sql = "SELECT gebruikersnaam FROM Gebruiker WHERE gebruikersnaam = :username";
+        //controleer of de gebruikersnaam nog niet bezet is
+        $sql = "SELECT count(gebruikersnaam) AS aantal FROM Gebruiker WHERE gebruikersnaam = :username";
         $sql = $dbh->prepare($sql);
         $sql->bindParam(':username', $username);
         $sql->execute();
         $username = $sql->fetch(PDO::FETCH_ASSOC);
 
-        if ($sql->rowCount() != 0) {
-            header("Location: {$_SERVER['HTTP_REFERER']}");
-            $_SESSION['messages'][] = 'Deze username is helaas al in gebruik';
-            exit ('Velden zijn hetzelfde');
+        if ($row = $sql->fetch()) {
+            if ($row['aantal'] != 0) {
+                $usernameCorrect = false;
+                //TODO: script melding
+            } else {
+                $usernameCorrect = true;
+            }
         }
 
 
-        $sql = "SELECT email FROM Gebruiker WHERE mail_adres = :email";
+        //controleer of de email nog niet bezet is
+        $sql = "SELECT count(email) AS aantal FROM Gebruiker WHERE mail_adres = :email";
         $sql = $dbh->prepare($sql);
         $sql->bindParam(':email', $email);
         $sql->execute();
         $email = $sql->fetch(PDO::FETCH_ASSOC);
 
-        if ($sql->rowCount() != 0) {
-            header("Location: {$_SERVER['HTTP_REFERER']}");
-            $_SESSION['messages'][] = 'Deze email is helaas al in gebruik';
-            exit ('Velden zijn hetzelfde');
-
+        if ($row = $sql->fetch()) {
+            if ($row['aantal'] != 0) {
+                //TODO: script melding
+                $emailUniek = false;
+            } else {
+                $emailUniek = true;
+            }
         }
     } catch
     (PDOException $e) {
         echo "Fout" . $e->getMessage();
     }
 
+    //controleer of het email wel geldig is
+    try {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailCorrect = false;
+            //TODO: script melding
+        } else {
+            if ($emailUniek) {
+                $emailCorrect = true;
+            }
+
+        }
+    } catch (PDOException $e) {
+        echo "Fout" . $e->getMessage();
+    }
+
+    if ($passwordCorrect && $usernameCorrect && $emailCorrect) {
+        $dataCorrect = true;
+    }
+
+    if ($dataCorrect) {
+
+
+        sanitizing_input($username, $firstname, $lastname, $firstAddress, $secondAddress, $postcode, $plaatsnaam, $securityQuestionAnswer, $email, $dbh);
+
+        $sql = "insert into Gebruiker ([gebruikersnaam], [voornaam], [achternaam], [adresregel1], [adresregel2], [postcode], [plaatsnaam], [land], [geboortedag], [mail_adres], [wachtwoord], [vraag], [antwoordtekst])
+        values (?, ?, ?, ?, ?, ?, ?,?,?,?,?,?,?)";
+        $query = $dbh->prepare($sql);
+        $query->execute(array($username, $firstname, $lastname, $firstAddress, $secondAddress, $postcode, $plaatsnaam, $country, $birthDate, $email, $passwordhash, $securityQuestion, $securityQuestionAnswer));
+
+
+        $_SESSION['regMelding'] = '
+    <script>UIkit.notification({message: \'Bedankt voor de registratie ' . $username . '!\', status: \'danger\'})</script>
+    ';
+
+
+        $_SESSION['messages'][] = "Bedankt voor uw registratie " . $firstname . "!";
+
+
+        $_SESSION['messages'][] = "Bedankt voor uw registratie " . $firstname . "!";
+        header("Location: ../login.php");
+
+    } else {
+        echo $username;
+        die();
+        $headerURL = "Location: ../verification.php?email=" . $_POST['email'] . "&hash=" . $_POST['hash'] . '&firstname=' . $firstname . '&lastname=' . $lastname . '&firstAddress=' . $firstAddress . '&secondAddress=' . $secondAddress . '&postalCode=' . $postcode . '&city=' . $plaatsnaam . '&birthDate=' . $birthDate . '&username=' . $username . '&securityQuestionAnswer=' . $securityQuestionAnswer;
+        if (!$usernameCorrect) {
+            $headerURL .= '&usernameError=1';
+        }
+        if (!$passwordCorrect) {
+            $headerURL .= '&passwordError=1';
+        }
+
+        if (!$emailUniek || !$emailCorrect) {
+            $emailError = 0;
+            if (!$emailUniek) {
+                $emailError = 1;
+            }
+            if (!$emailCorrect) {
+                $emailError = 2;
+            }
+            $headerURL .= 'emailError=' . $emailError;
+        }
+
+        header($headerURL);
+    }
 }
 
- ?>
+function sanitizing_input($username, $firstname, $lastname, $eersteAdres, $tweedeAdres, $postcode, $plaatsnaam, $antwoord, $email, $dbh)
+{
+    $firstname = trim($firstname);
+    $lastname = trim($lastname);
+    $firstname = ucfirst($firstname);
+    $lastname = ucfirst($lastname);
+    $firstname = htmlspecialchars($firstname);
+    $lastname = htmlspecialchars($lastname);
+    $eersteAdres = htmlspecialchars($eersteAdres);
+    $tweedeAdres = htmlspecialchars($tweedeAdres);
+    $postcode = htmlspecialchars($postcode);
+    $plaatsnaam = htmlspecialchars($plaatsnaam);
+    $antwoord = htmlspecialchars($antwoord);
+
+
+}
+
+?>
--- EenmaalAndermaal/verification.php
+++ EenmaalAndermaal/verification.php
@@ -6,120 +6,128 @@ include('scripts/country.php');
 include('scripts/database-connect.php');
 include('scripts/question.php');
 
-activateAccount($dbh);
 
-function activateAccount($dbh)
-{
 $match = 0;
 if (isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])) {
     // Verify data
     $email = $_GET['email']; // Set email variable
     $hash = $_GET['hash']; // Set hash variable
-}
 
-$search = $dbh->query("SELECT email, hash FROM Verificatie WHERE email='" . $email . "' AND hash='" . $hash . "'");
-while ($row = $search->fetch()) {
-    $match ++;
-}
-
-if ($match > 0){
-
-    $dbh->query("UPDATE Verificatie SET isGeactiveerd='1' WHERE email='" . $email . "' AND hash='" . $hash . "'");
-} else {
-
-}
-
-}
-$email = $_GET['email'];
-
-echo'
+    $form = '
 <body>
-<form action="scripts/newUser.php?email='.$email.'" method="post" >
+<form action="scripts/newUser.php?email=' . $email . '" method="post" >
+        
     <div class="uk-card uk-card-default uk-card-body uk-width-1-3@m uk-margin-auto uk-margin-top uk-margin-bottom">
       <h3 class="uk-card-title uk-text-center uk-margin-bottom">Registreren bij EenmaalAndermaal</h3>
-';
-?>
-    <div class="uk-margin">
-        <input class="uk-input" type="text" placeholder="Voornaam" name="Voornaam"required>
-    </div>
 
-    <div class="uk-margin">
-        <input class="uk-input" type="text" placeholder="Achternaam" name="Achternaam"required>
-    </div>
+    <input type="text" name="email" value="' . $_GET['email'] . '" hidden>
+    <input type="text" name="hash" value="' . $_GET['hash'] . '" hidden>
 
-    <div class="uk-margin">
-        <input class="uk-input" type="text" placeholder="Eerste adres" name="EersteAdres"required>
-    </div>
+<div class="uk-margin">
+    <input class="uk-input" type="text" placeholder="Voornaam" name="Voornaam"'; if(isset($_GET['firstname'])){$form .='value="'.$_GET['firstname'].'"';}
+    $form.='required>
+</div>
 
-    <div class="uk-margin">
-        <input class="uk-input" type="text" placeholder="Tweede adres" name="TweedeAdres">
-    </div>
+<div class="uk-margin">
+    <input class="uk-input" type="text" placeholder="Achternaam" name="Achternaam"'; if(isset($_GET['lastname'])){$form .='value="'.$_GET['lastname'].'"';}
+    $form.='required>
+</div>
 
-    <div class="uk-margin">
-        <input class="uk-input" type="text" placeholder="Postcode" name="Postcode"required>
-    </div>
+<div class="uk-margin">
+    <input class="uk-input" type="text" placeholder="Eerste adres" name="EersteAdres"'; if(isset($_GET['firstAddress'])){$form .='value="'.$_GET['firstAddress'].'"';}
+    $form.='required>
+</div>
 
-    <div class="uk-margin">
-        <input class="uk-input" type="text" placeholder="Plaatsnaam" name="Plaatsnaam"required>
-    </div>
+<div class="uk-margin">
+    <input class="uk-input" type="text" placeholder="Tweede adres (optioneel)" name="TweedeAdres"'; if(isset($_GET['secondAddress'])){$form .='value="'.$_GET['secondAddress'].'"';}
+    $form.='>
+</div>
 
-    <div class="uk-margin">
-        <select class="uk-select" name="Land"required >
-          <option value="Nederland">Nederland</option>
-  <?php
-        Get_country($dbh);
-      ?>
-        </select>
-    </div>
+<div class="uk-margin">
+    <input class="uk-input" type="text" placeholder="Postcode" name="Postcode"'; if(isset($_GET['postalCode'])){$form .='value="'.$_GET['postalCode'].'"';}
+    $form.='required>
+</div>
 
-    <div class="uk-margin">
-        <input class="uk-input" type="date" name="Datum"required>
-    </div>
+<div class="uk-margin">
+    <input class="uk-input" type="text" placeholder="Plaatsnaam" name="Plaatsnaam"'; if(isset($_GET['city'])){$form .='value="'.$_GET['city'].'"';}
+    $form.='required>
+</div>
 
-    <div class="uk-margin">
+<div class="uk-margin">
+    <select class="uk-select" name="Land"required >
+        <option value="Nederland">Nederland</option>
+       ' .
+        Get_country($dbh)
+        . '
+    </select>
+</div>
+
+<div class="uk-margin">
+    <input class="uk-input" type="date" name="Datum"'; if(isset($_GET['birthDate'])){$form .='value="'.$_GET['birthDate'].'"';}
+    $form.='required>
+</div>
+
+<div class="uk-margin">
     <div class="uk-inline uk-width-1-1">
         <span class="uk-form-icon" uk-icon="icon: user"></span>
-        <input class="uk-input" type="text" placeholder="Gebruikersnaam" name="Gebruikersnaam"required>
-        </div>
+        <input class="uk-input" type="text" placeholder="Gebruikersnaam" name="Gebruikersnaam" '; if(isset($_GET['username'])){$form .='value="'.$_GET['username'].'"';}
+    $form.='required>
     </div>
+</div>
 
+<div class="uk-margin">
+    <div class="uk-inline uk-width-1-1">
+        <span class="uk-form-icon uk-form-icon" uk-icon="icon: lock"></span>
+        <input class="uk-input" type="password" placeholder="Wachtwoord" name="Wachtwoord"required>
+    </div>
+</div>
+
+<div class="uk-margin">
+    <div class="uk-inline uk-width-1-1">
+        <span class="uk-form-icon uk-form-icon" uk-icon="icon: lock"></span>
+        <input class="uk-input" type="password" placeholder="Wachtwoord bevestigen" name="Wachtwoord_bevestigen" required>
+    </div>
+</div>
+
+<div class="uk-margin">
+    <select class="uk-select" name="vraag"required>' .
+        Get_question($dbh) . '
+    </select>
     <div class="uk-margin">
-        <div class="uk-inline uk-width-1-1">
-            <span class="uk-form-icon uk-form-icon" uk-icon="icon: lock"></span>
-            <input class="uk-input" type="password" placeholder="Wachtwoord" name="Wachtwoord"required>
-        </div>
-    </div>
 
-    <div class="uk-margin">
-        <div class="uk-inline uk-width-1-1">
-            <span class="uk-form-icon uk-form-icon" uk-icon="icon: lock"></span>
-            <input class="uk-input" type="password" placeholder="Wachtwoord_bevestigen"required>
-        </div>
-    </div>
-
-    <div class="uk-margin">
-        <select class="uk-select" name="vraag"required>
-        <?php
-        Get_question($dbh);
-        ?>
-      </select>
-        <div class="uk-margin">
-
-            <input class="uk-input" type="text" placeholder="Antwoord" name="Antwoord"required>
-
-          </div>
-    </div>
-
-    <div class="uk-margin">
-        <div class="uk-inline uk-width-1-1">
-            <input class="uk-input uk-button-primary" type="submit" name = "submit"  value="versturen">
-        </div>
-    </div>
-
+        <input class="uk-input" type="text" placeholder="Antwoord" name="Antwoord"'; if(isset($_GET['securityQuestionAnswer'])){$form .='value="'.$_GET['securityQuestionAnswer'].'"';}
+    $form.='required>
 
     </div>
+</div>
+
+<div class="uk-margin">
+    <div class="uk-inline uk-width-1-1">
+        <input class="uk-input uk-button-primary" type="submit" name = "submit"  value="versturen">
+    </div>
+</div>
+
+
+</div>
 </form>
-</body>
-<?php
+</body>';
+
+    echo $form;
+
+} else {
+    header('Location: registration.php');
+}
+
+//$search = $dbh->query("SELECT email, hash FROM Verificatie WHERE email='" . $email . "' AND hash='" . $hash . "'");
+//while ($row = $search->fetch()) {
+//    $match ++;
+//}
+//
+//if ($match > 0){
+//
+//    $dbh->query("UPDATE Verificatie SET isGeactiveerd='1' WHERE email='" . $email . "' AND hash='" . $hash . "'");
+//}
+
+
 require_once('scripts/footer.php');
-?>
+
