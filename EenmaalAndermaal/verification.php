5d08388d05cb6682669028d27ebf7dcfa73870de
synolog1x <sanderrdtc@gmail.com>
2018-05-22 14:25:55 +0200

    verificatie email en hash check

--- EenmaalAndermaal/verification.php
+++ EenmaalAndermaal/verification.php
@@ -7,34 +7,32 @@ include('scripts/database-connect.php');
 include('scripts/question.php');
 
 
-<<<<<<< HEAD
-$match = 0;
-
-=======
 $validURL = false;
-/*
->>>>>>> 7d6d44b06efe95b6f56ac4981d1003d4e796a514
+echo "hallo";
+
 if (isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])) {
     // Verify data
-    //TODO CONTROLEER MET DATABASE
     $email = $_GET['email']; // Set email variable
     $hash = $_GET['hash']; // Set hash variable
 
-<<<<<<< HEAD
-=======
-    $sql = "SELECT COUNT(mail) AS aantal FROM Verificatie WHERE mail like '$email' AND hash like '$hash';
-    $query = $dbh->prepare($query);
-    $query->execute();
-    if($query->fetch()){
-    if($row['aantal'] == 1){
-    $validURL = true;
+    try {
+        $sql = "SELECT COUNT (email) AS aantal FROM Verificatie WHERE email like '" . $email . "' AND hash like '" . $hash . "'";
+        var_dump($sql);
+        $query = $dbh->prepare($sql);
+        $query->execute();
+        if ($row = $query->fetch()) {
+            echo $row['aantal'];
+            if ($row['aantal'] == 1) {
+                $validURL = true;
+            }
+        }
+    } catch (PDOException $e) {
+        echo "Fout" . $e->getMessage();
     }
-    }
-*/
-//}
+}
 
-//if($validURL){
->>>>>>> 7d6d44b06efe95b6f56ac4981d1003d4e796a514
+
+if ($validURL) {
     $form = '
 
 <body>
@@ -46,38 +44,56 @@ if (isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && 
     <p class="uk-icon-font-awesome uk-margin-remove">* = Verplicht veld</p>
 <div class="uk-margin uk-form-horizontal">
     <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Voornaam* :</label>
-    <input class="uk-input uk-width-2-3" type="text" placeholder="Voornaam" name="Voornaam"'; if(isset($_GET['firstname'])){$form .='value="'.$_GET['firstname'].'"';}
-    $form.='required>
+    <input class="uk-input uk-width-2-3" type="text" placeholder="Voornaam" name="Voornaam"';
+    if (isset($_GET['firstname'])) {
+        $form .= 'value="' . $_GET['firstname'] . '"';
+    }
+    $form .= 'required>
 </div>
 
 <div class="uk-margin uk-form-horizontal">
     <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Achternaam">Achternaam* :</label>
-    <input class="uk-input uk-width-2-3" type="text" placeholder="Achternaam" name="Achternaam"'; if(isset($_GET['lastname'])){$form .='value="'.$_GET['lastname'].'"';}
-    $form.='required>
+    <input class="uk-input uk-width-2-3" type="text" placeholder="Achternaam" name="Achternaam"';
+    if (isset($_GET['lastname'])) {
+        $form .= 'value="' . $_GET['lastname'] . '"';
+    }
+    $form .= 'required>
 </div>
 
 <div class="uk-margin uk-form-horizontal">
     <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Eerste Adres* :</label>
-    <input class="uk-input uk-width-2-3" type="text" placeholder="Eerste adres" name="EersteAdres"'; if(isset($_GET['firstAddress'])){$form .='value="'.$_GET['firstAddress'].'"';}
-    $form.='required>
+    <input class="uk-input uk-width-2-3" type="text" placeholder="Eerste adres" name="EersteAdres"';
+    if (isset($_GET['firstAddress'])) {
+        $form .= 'value="' . $_GET['firstAddress'] . '"';
+    }
+    $form .= 'required>
 </div>
 
 <div class="uk-margin uk-form-horizontal">
     <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Tweede Adres :</label>
-    <input class="uk-input uk-width-2-3" type="text" placeholder="Tweede adres (optioneel)" name="TweedeAdres"'; if(isset($_GET['secondAddress'])){$form .='value="'.$_GET['secondAddress'].'"';}
-    $form.='>
+    <input class="uk-input uk-width-2-3" type="text" placeholder="Tweede adres (optioneel)" name="TweedeAdres"';
+    if (isset($_GET['secondAddress'])) {
+        $form .= 'value="' . $_GET['secondAddress'] . '"';
+    }
+    $form .= '>
 </div>
 
 <div class="uk-margin uk-form-horizontal">
     <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Postcode* :</label>
-    <input class="uk-input uk-width-2-3" type="text" placeholder="Postcode" name="Postcode"'; if(isset($_GET['postalCode'])){$form .='value="'.$_GET['postalCode'].'"';}
-    $form.='required>
+    <input class="uk-input uk-width-2-3" type="text" placeholder="Postcode" name="Postcode"';
+    if (isset($_GET['postalCode'])) {
+        $form .= 'value="' . $_GET['postalCode'] . '"';
+    }
+    $form .= 'required>
 </div>
 
 <div class="uk-margin uk-form-horizontal">
     <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Plaatsnaam* :</label>
-    <input class="uk-input uk-width-2-3" type="text" placeholder="Plaatsnaam" name="Plaatsnaam"'; if(isset($_GET['city'])){$form .='value="'.$_GET['city'].'"';}
-    $form.='required>
+    <input class="uk-input uk-width-2-3" type="text" placeholder="Plaatsnaam" name="Plaatsnaam"';
+    if (isset($_GET['city'])) {
+        $form .= 'value="' . $_GET['city'] . '"';
+    }
+    $form .= 'required>
 </div>
 
 <div class="uk-margin uk-form-horizontal">
@@ -92,30 +108,48 @@ if (isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && 
 
 <div class="uk-margin uk-form-horizontal">
     <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Geboortedatum* :</label>
-    <input class="uk-input uk-width-2-3" type="date" name="Datum"'; if(isset($_GET['birthDate'])){$form .='value="'.$_GET['birthDate'].'"';}
-    $form.='required>
+    <input class="uk-input uk-width-2-3" type="date" name="Datum"';
+    if (isset($_GET['birthDate'])) {
+        $form .= 'value="' . $_GET['birthDate'] . '"';
+    }
+    $form .= 'required>
 </div>';
-    if(isset($_GET['usernameError']) && $_GET['usernameError'] == 1){$form.='<p class="uk-text-danger">Deze gebruikersnaam is al in gebruik.</p>';}
+    if (isset($_GET['usernameError']) && $_GET['usernameError'] == 1) {
+        $form .= '<p class="uk-text-danger">Deze gebruikersnaam is al in gebruik.</p>';
+    }
 
-    $form.='
+    $form .= '
 <div class="uk-margin uk-form-horizontal">
     <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Gebruikersnaam* :</label>
     <div class="uk-inline uk-width-2-3">
         <span class="uk-form-icon" uk-icon="icon: user"></span>
-        <input class="uk-input'; if(isset($_GET['usernameError']) && $_GET['usernameError'] == 1){$form.=' uk-form-danger';} $form.= '" type="text" placeholder="Gebruikersnaam" name="Gebruikersnaam" '; if(isset($_GET['username'])){$form .='value="'.$_GET['username'].'"';}
-    $form.='required>
+        <input class="uk-input';
+    if (isset($_GET['usernameError']) && $_GET['usernameError'] == 1) {
+        $form .= ' uk-form-danger';
+    }
+    $form .= '" type="text" placeholder="Gebruikersnaam" name="Gebruikersnaam" ';
+    if (isset($_GET['username'])) {
+        $form .= 'value="' . $_GET['username'] . '"';
+    }
+    $form .= 'required>
 
     </div>
 </div>
 
 ';
-        if(isset($_GET['passwordError']) && $_GET['passwordError'] == 1){$form.='<p class="uk-text-danger">De opgegeven wachtwoorden komen niet overeen.</p>';}
-$form.='
+    if (isset($_GET['passwordError']) && $_GET['passwordError'] == 1) {
+        $form .= '<p class="uk-text-danger">De opgegeven wachtwoorden komen niet overeen.</p>';
+    }
+    $form .= '
 <div class="uk-margin uk-form-horizontal">
     <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Wachtwoord* :</label>
     <div class="uk-inline uk-width-2-3">
         <span class="uk-form-icon uk-form-icon" uk-icon="icon: lock"></span>
-        <input class="uk-input'; if(isset($_GET['passwordError']) && $_GET['passwordError'] == 1){$form.=' uk-form-danger';} $form.= '" type="password" placeholder="Wachtwoord" name="Wachtwoord" pattern="(?=^.{7,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required>
+        <input class="uk-input';
+    if (isset($_GET['passwordError']) && $_GET['passwordError'] == 1) {
+        $form .= ' uk-form-danger';
+    }
+    $form .= '" type="password" placeholder="Wachtwoord" name="Wachtwoord" pattern="(?=^.{7,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required>
     </div>
 </div>
 
@@ -123,7 +157,11 @@ $form.='
     <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Wachtwoord Bevestigen* :</label>
     <div class="uk-inline uk-width-2-3">
         <span class="uk-form-icon uk-form-icon" uk-icon="icon: lock"></span>
-        <input class="uk-input'; if(isset($_GET['passwordError']) && $_GET['passwordError'] == 1){$form.=' uk-form-danger';} $form.= '" type="password" placeholder="Wachtwoord bevestigen" name="Wachtwoord_bevestigen" pattern="(?=^.{7,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required>
+        <input class="uk-input';
+    if (isset($_GET['passwordError']) && $_GET['passwordError'] == 1) {
+        $form .= ' uk-form-danger';
+    }
+    $form .= '" type="password" placeholder="Wachtwoord bevestigen" name="Wachtwoord_bevestigen" pattern="(?=^.{7,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required>
 
     </div>
 </div>
@@ -136,8 +174,11 @@ $form.='
     </select>
     <div class="uk-margin">
         <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Antwoord* :</label>
-        <input class="uk-input uk-width-2-3" type="text" placeholder="Antwoord" name="Antwoord"'; if(isset($_GET['securityQuestionAnswer'])){$form .='value="'.$_GET['securityQuestionAnswer'].'"';}
-        $form.='required>
+        <input class="uk-input uk-width-2-3" type="text" placeholder="Antwoord" name="Antwoord"';
+    if (isset($_GET['securityQuestionAnswer'])) {
+        $form .= 'value="' . $_GET['securityQuestionAnswer'] . '"';
+    }
+    $form .= 'required>
     </div>
 </div>
 
@@ -155,22 +196,13 @@ $form.='
 
     echo $form;
 
-}
-else {
-    //TODO: uikit script melding: je kan alleen op deze pagina komen via de registratie-email
-<<<<<<< HEAD
+} else {
+
+    $_SESSION['emailMelding'] = '
+        <script style="border-radius: 25px;">UIkit.notification({message: \' <span uk-icon="icon: warning"></span> Deze verificatielink is niet geldig.\', status: \'danger\'})</script>';
     header('Location: registration.php');
 }
-=======
-$_SESSION['emailMelding'] = '
-        <script style="border-radius: 25px;">UIkit.notification({message: \' <span uk-icon="icon: warning"></span> Deze verificatielink is niet geldig.\', status: \'danger\'})</script>';
-    //header('Location: registration.php');
-//}
 
-
-
-
->>>>>>> 7d6d44b06efe95b6f56ac4981d1003d4e796a514
 
 require_once('scripts/footer.php');
 
