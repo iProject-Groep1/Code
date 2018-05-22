2dd5c1133bbabb94c7aa92fbdb5805d9362bd3a7
LarsBuijs00 <lars@buijsnet.com>
2018-05-22 13:34:48 +0200

    fixed css

--- EenmaalAndermaal/verification.php
+++ EenmaalAndermaal/verification.php
@@ -19,46 +19,52 @@ if (isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && 
 <body>
 
 <form action="scripts/newUser.php?email=' . $email . '" method="post" >
-        
-    <div class="uk-card uk-card-default uk-card-body uk-width-1-2@m uk-margin-auto uk-margin-top uk-margin-bottom">
-      <h3 class="uk-card-title uk-text-center uk-margin-bottom">Registreren bij EenmaalAndermaal</h3>
+              <h3 class="uk-card-title uk-text-center uk-margin-bottom-remove">Registreren bij EenmaalAndermaal</h3>
+    <div class="uk-card uk-card-default uk-card-body uk-width-2-5@m uk-margin-auto uk-margin-top-remove uk-margin-bottom">
+
 
     <input type="text" name="email" value="' . $_GET['email'] . '" hidden>
     <input type="text" name="hash" value="' . $_GET['hash'] . '" hidden>
-
+<p class="uk-icon-font-awesome">* : Verplicht veld</p>
 <div class="uk-margin uk-form-horizontal">
-    <label class="uk-form-label" for="Voornaam">Voornaam *:</label>
-    <input class="uk-input" type="text" placeholder="Voornaam" name="Voornaam"'; if(isset($_GET['firstname'])){$form .='value="'.$_GET['firstname'].'"';}
+    <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Voornaam* :</label>
+    <input class="uk-input uk-width-2-3" type="text" placeholder="Voornaam" name="Voornaam"'; if(isset($_GET['firstname'])){$form .='value="'.$_GET['firstname'].'"';}
     $form.='required>
 </div>
 
 <div class="uk-margin uk-form-horizontal">
-    <input class="uk-input" type="text" placeholder="Achternaam" name="Achternaam"'; if(isset($_GET['lastname'])){$form .='value="'.$_GET['lastname'].'"';}
+    <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Achternaam">Achternaam* :</label>
+    <input class="uk-input uk-width-2-3" type="text" placeholder="Achternaam" name="Achternaam"'; if(isset($_GET['lastname'])){$form .='value="'.$_GET['lastname'].'"';}
     $form.='required>
 </div>
 
 <div class="uk-margin uk-form-horizontal">
-    <input class="uk-input" type="text" placeholder="Eerste adres" name="EersteAdres"'; if(isset($_GET['firstAddress'])){$form .='value="'.$_GET['firstAddress'].'"';}
+    <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Eerste Adres* :</label>
+    <input class="uk-input uk-width-2-3" type="text" placeholder="Eerste adres" name="EersteAdres"'; if(isset($_GET['firstAddress'])){$form .='value="'.$_GET['firstAddress'].'"';}
     $form.='required>
 </div>
 
 <div class="uk-margin uk-form-horizontal">
-    <input class="uk-input" type="text" placeholder="Tweede adres (optioneel)" name="TweedeAdres"'; if(isset($_GET['secondAddress'])){$form .='value="'.$_GET['secondAddress'].'"';}
+    <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Tweede Adres :</label>
+    <input class="uk-input uk-width-2-3" type="text" placeholder="Tweede adres (optioneel)" name="TweedeAdres"'; if(isset($_GET['secondAddress'])){$form .='value="'.$_GET['secondAddress'].'"';}
     $form.='>
 </div>
 
 <div class="uk-margin uk-form-horizontal">
-    <input class="uk-input" type="text" placeholder="Postcode" name="Postcode"'; if(isset($_GET['postalCode'])){$form .='value="'.$_GET['postalCode'].'"';}
+    <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Postcode* :</label>
+    <input class="uk-input uk-width-2-3" type="text" placeholder="Postcode" name="Postcode"'; if(isset($_GET['postalCode'])){$form .='value="'.$_GET['postalCode'].'"';}
     $form.='required>
 </div>
 
 <div class="uk-margin uk-form-horizontal">
-    <input class="uk-input" type="text" placeholder="Plaatsnaam" name="Plaatsnaam"'; if(isset($_GET['city'])){$form .='value="'.$_GET['city'].'"';}
+    <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Plaatsnaam* :</label>
+    <input class="uk-input uk-width-2-3" type="text" placeholder="Plaatsnaam" name="Plaatsnaam"'; if(isset($_GET['city'])){$form .='value="'.$_GET['city'].'"';}
     $form.='required>
 </div>
 
 <div class="uk-margin uk-form-horizontal">
-    <select class="uk-select" name="Land"required >
+    <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Land* :</label>
+    <select class="uk-select uk-width-2-3" name="Land"required >
         <option value="Nederland">Nederland</option>
        ' .
         Get_country($dbh)
@@ -67,14 +73,16 @@ if (isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && 
 </div>
 
 <div class="uk-margin uk-form-horizontal">
-    <input class="uk-input" type="date" name="Datum"'; if(isset($_GET['birthDate'])){$form .='value="'.$_GET['birthDate'].'"';}
+    <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Geboortedatum* :</label>
+    <input class="uk-input uk-width-2-3" type="date" name="Datum"'; if(isset($_GET['birthDate'])){$form .='value="'.$_GET['birthDate'].'"';}
     $form.='required>
 </div>';
     if(isset($_GET['usernameError']) && $_GET['usernameError'] == 1){$form.='<p class="uk-text-danger">Deze gebruikersnaam is al in gebruik.</p>';}
 
     $form.='
 <div class="uk-margin uk-form-horizontal">
-    <div class="uk-inline uk-width-1-1">
+    <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Gebruikersnaam* :</label>
+    <div class="uk-inline uk-width-2-3">
         <span class="uk-form-icon" uk-icon="icon: user"></span>
         <input class="uk-input'; if(isset($_GET['usernameError']) && $_GET['usernameError'] == 1){$form.=' uk-form-danger';} $form.= '" type="text" placeholder="Gebruikersnaam" name="Gebruikersnaam" '; if(isset($_GET['username'])){$form .='value="'.$_GET['username'].'"';}
     $form.='required>
@@ -86,14 +94,16 @@ if (isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && 
         if(isset($_GET['passwordError']) && $_GET['passwordError'] == 1){$form.='<p class="uk-text-danger">De opgegeven wachtwoorden komen niet overeen.</p>';}
 $form.='
 <div class="uk-margin uk-form-horizontal">
-    <div class="uk-inline uk-width-1-1">
+    <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Wachtwoord* :</label>
+    <div class="uk-inline uk-width-2-3">
         <span class="uk-form-icon uk-form-icon" uk-icon="icon: lock"></span>
         <input class="uk-input'; if(isset($_GET['passwordError']) && $_GET['passwordError'] == 1){$form.=' uk-form-danger';} $form.= '" type="password" placeholder="Wachtwoord" name="Wachtwoord"required>
     </div>
 </div>
 
 <div class="uk-margin uk-form-horizontal">
-    <div class="uk-inline uk-width-1-1">
+    <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Wachtwoord Bevestigen* :</label>
+    <div class="uk-inline uk-width-2-3">
         <span class="uk-form-icon uk-form-icon" uk-icon="icon: lock"></span>
         <input class="uk-input'; if(isset($_GET['passwordError']) && $_GET['passwordError'] == 1){$form.=' uk-form-danger';} $form.= '" type="password" placeholder="Wachtwoord bevestigen" name="Wachtwoord_bevestigen" required>
 
@@ -102,19 +112,21 @@ $form.='
 
 
 <div class="uk-margin uk-form-horizontal">
-    <select class="uk-select" name="vraag"required>' .
+    <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Veiligheidsvraag* :</label>
+    <select class="uk-select uk-width-2-3" name="vraag"required>' .
         Get_question($dbh) . '
     </select>
     <div class="uk-margin">
-        <input class="uk-input" type="text" placeholder="Antwoord" name="Antwoord"'; if(isset($_GET['securityQuestionAnswer'])){$form .='value="'.$_GET['securityQuestionAnswer'].'"';}
+        <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="Voornaam">Antwoord* :</label>
+        <input class="uk-input uk-width-2-3" type="text" placeholder="Antwoord" name="Antwoord"'; if(isset($_GET['securityQuestionAnswer'])){$form .='value="'.$_GET['securityQuestionAnswer'].'"';}
         $form.='required>
     </div>
 </div>
 
 
-<div class="uk-margin">
-    <div class="uk-inline uk-width-1-1">
-        <input class="uk-input uk-button-primary" type="submit" name = "submit"  value="versturen">
+<div class="uk-margin uk-flex uk-flex-center ">
+    <div class="uk-inline uk-width-1-2">
+        <input class="uk-input uk-button-primary" type="submit" name = "submit"  value="Versturen">
     </div>
 </div>
 
