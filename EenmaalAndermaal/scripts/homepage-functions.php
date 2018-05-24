0601e2acc6191dc30de1333dbf9ec4e2d2882b63
synolog1x <sanderrdtc@gmail.com>
2018-05-24 10:15:30 +0200

    laat startprijs zien als er geen hoogstebod is

--- EenmaalAndermaal/scripts/homepage-functions.php
+++ EenmaalAndermaal/scripts/homepage-functions.php
@@ -123,7 +123,11 @@ function getHomepageCards($dbh, $query)
         $stmt = $dbh->prepare($query); /* prepared statement */
         $stmt->execute(); /* stuurt alles naar de server */
         while ($results = $stmt->fetch()) {
-            $itemCards .= createItemScript($results['titel'], $results['looptijdEindmoment'], $results['bestandsnaam'], $results['hoogsteBod'], $results['voorwerpnummer']);
+            $price = $results['hoogsteBod'];
+            if(is_null($price)){
+                $price = getStartPrice($dbh, $results['voorwerpnummer']);
+            }
+            $itemCards .= createItemScript($results['titel'], $results['looptijdEindmoment'], $results['bestandsnaam'], $price, $results['voorwerpnummer']);
         }
     } catch (PDOException $e) {
         echo "Fout" . $e->getMessage();
