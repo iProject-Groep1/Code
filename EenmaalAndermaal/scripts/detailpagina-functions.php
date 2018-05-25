2ecf005f91cbd1ec110ce41a6c3bc40b3c40cf77
synolog1x <sanderrdtc@gmail.com>
2018-05-25 10:27:07 +0200

    toon huidige item niet in relevante items

--- EenmaalAndermaal/scripts/detailpagina-functions.php
+++ EenmaalAndermaal/scripts/detailpagina-functions.php
@@ -1,10 +1,11 @@
 <?php
 
 
-function setRelevantItems($dbh, $rubriek_op_laagste_Niveau)
+function setRelevantItems($dbh, $rubriek_op_laagste_Niveau, $voorwerp)
 {
     try {
-        $stmt = $dbh->prepare("SELECT top 4 v.voorwerpnummer, v.titel, v.looptijdEindmoment, (SELECT TOP 1 filenaam FROM bestand f WHERE v.voorwerpnummer = f.voorwerp) AS bestandsnaam, MAX(Bodbedrag) AS hoogsteBod, CURRENT_TIMESTAMP AS serverTijd FROM Voorwerp v join Bod b ON v.voorwerpnummer = b.voorwerp join VoorwerpInRubriek r ON v.voorwerpnummer = r.voorwerp  WHERE r.rubriek_op_laagste_Niveau = :rubriek_op_laagste_Niveau GROUP BY Voorwerpnummer, titel, looptijdEindmoment"); /* prepared statement */
+        $stmt = $dbh->prepare("SELECT top 4 v.voorwerpnummer, v.titel, v.looptijdEindmoment, (SELECT TOP 1 filenaam FROM bestand f WHERE v.voorwerpnummer = f.voorwerp) AS bestandsnaam, MAX(Bodbedrag) AS hoogsteBod, CURRENT_TIMESTAMP AS serverTijd FROM Voorwerp v join Bod b ON v.voorwerpnummer = b.voorwerp join VoorwerpInRubriek r ON v.voorwerpnummer = r.voorwerp  WHERE r.rubriek_op_laagste_Niveau = :rubriek_op_laagste_Niveau AND voorwerpnummer != :voorwerpnummer GROUP BY Voorwerpnummer, titel, looptijdEindmoment"); /* prepared statement */
+       $stmt->bindValue(":voorwerpnummer", $voorwerp, PDO::PARAM_STR);
         $stmt->bindValue(":rubriek_op_laagste_Niveau", $rubriek_op_laagste_Niveau, PDO::PARAM_STR); /* helpt tegen SQL injection */
         $stmt->execute(); /* stuurt alles naar de server */
         while ($results = $stmt->fetch()) {
@@ -24,7 +25,7 @@ function getRelevantItems($dbh, $voorwerp)
         $stmt->bindValue(":voorwerp", $voorwerp, PDO::PARAM_STR); /* helpt tegen SQL injection */
         $stmt->execute(); /* stuurt alles naar de server */
         while ($results = $stmt->fetch()) {
-            setRelevantItems($dbh, $results['rubriek_op_laagste_Niveau']);
+            setRelevantItems($dbh, $results['rubriek_op_laagste_Niveau'], $voorwerp);
         }
     } catch (PDOException $e) {
         echo "Fout" . $e->getMessage();
