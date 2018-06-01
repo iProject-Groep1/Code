632878e9651b394d984a32f1772ab75e56e584a9
synolog1x <sanderrdtc@gmail.com>
2018-06-01 15:47:06 +0200

    code formatting

--- EenmaalAndermaal/scripts/question.php
+++ EenmaalAndermaal/scripts/question.php
@@ -7,19 +7,15 @@ $return = '';
   try {
       $stmt = $dbh->prepare("select [vraagnummer], [vraagtekst] from Vraag"); /* prepared statement */
       $stmt->execute(); /* stuurt alles naar de server */
-
       while ($row = $stmt->fetch()){
-
         $return .= '<option value="'. $row ['vraagnummer'] .'"> '.$row['vraagtekst'].'</option>';
-        
        }
-
       }
    catch (PDOException $e) {
       echo "Fout" . $e->getMessage();
        header('Location: errorpage.php?err=500');
   }
   return $return;
-};
+}
 
  ?>
