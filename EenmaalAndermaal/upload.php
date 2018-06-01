325223f702c38555868e00cf667a7940fd092c3f
TBeeren <tim.beerenn@gmail.com>
2018-06-01 15:45:53 +0200

    Uploaden werkt bijna

--- EenmaalAndermaal/upload.php
+++ EenmaalAndermaal/upload.php
@@ -22,10 +22,11 @@ if (isset($_SESSION['username']) && !empty($_SESSION['username']) && isset($_GET
     }
 
     $Rubriek = $_GET['Rubriek'];
+    $Rubrieknr = $_GET['Rubrieknr'];
 
-echo '
+    echo '
     <h1 class="uk-center-upload">Plaats Advertentie</h1>
-    <p class=" uk-center-upload ">'.$Rubriek . ' </p>
+    <p class=" uk-center-upload ">' . $Rubriek . ' </p>
     <div class="uk-align-left profile-sidebar uk-align-center@m uk-display-block uk-width-1-2@s uk-width-1-6@m">
         <ul class="uk-nav-default uk-nav-parent-icon uk-nav" uk-nav="">
             <li class="uk-parent uk-open">
@@ -46,55 +47,55 @@ echo '
             <img class="uk-display-block" src="images/placeholde-img.png" alt="placeholder" width="300">
         </div>
         <div class="uk-display-inline-block uk-width-1-2@s uk-width-1-3@m">
-            <form class="uk-form-horizontal uk-margin-large">
+            <form class="uk-form-horizontal uk-margin-large" action="scripts/placeItem.php?Rubrieknr='.$Rubrieknr.'" method="post">
                 <div class="uk-margin">
                     <label class="uk-form-label" for="form-horizontal-text">Titel</label>
                     <div class="uk-form-controls">
                         <input class="uk-input" id="form-horizontal-text" type="text"
-                               placeholder="Titel">
+                               placeholder="Titel" name="Titel">
                     </div>
                 </div>
                 <div class="uk-margin">
                     <label class="uk-form-label" for="form-horizontal-text">Startprijs</label>
                     <div class="uk-form-controls">
-                        <input class="uk-input" id="form-horizontal-text" type="text"
-                               placeholder="0">
+                        <input class="uk-input" id="form-horizontal-text" type="number" min="0" step="any"
+                               placeholder="0" name="Startprijs">
                     </div>
                 </div>
                 <div class="uk-margin">
                     <label class="uk-form-label" for="form-horizontal-text">Verzendkosten</label>
                     <div class="uk-form-controls">
-                        <input class="uk-input" id="form-horizontal-text" type="text"
-                               placeholder="0">
+                        <input class="uk-input" id="form-horizontal-text" type="number" min="0" step="any"
+                               placeholder="0" name="Verzendkosten">
                     </div>
                 </div>
                 <div class="uk-margin">
                     <label class="uk-form-label" for="form-horizontal-text">Betalingswijze</label>
                     <div class="uk-form-controls">
+                    <!-- TODO: Haal alle betaalwijze uit de database en laad deze in een <ul> -->
                         <input class="uk-input" id="form-horizontal-text" type="text"
-                               placeholder="IDEAL">
+                               placeholder="IDEAL" name="Betalingswijze">
                     </div>
                 </div>
                 <div class="uk-margin">
                     <label class="uk-form-label" for="form-horizontal-select">Veilingtijd</label>
                     <div class="uk-form-controls">
-                        <select class="uk-select" id="form-horizontal-select">
-                            <option default>1 Dag</option>
-                            <option>3 Dagen</option>
-                            <option>5 Dagen</option>
-                            <option>7 Dagen</option>
-                            <option>10 Dagen</option>
+                        <select class="uk-select" id="form-horizontal-select" name="Veilingtijd">
+                            <option>1</option>
+                            <option>3</option>
+                            <option>5</option>
+                            <option default>7</option>
+                            <option>10</option>
                         </select>
                     </div>
                 </div>
-            </form>
         </div>
         <div class="uk-card-refactor auctions-reset-margin uk-display-inline-block uk-margin-top-zero">
             <div class="js-upload uk-placeholder uk-text-center uk-upload-picture">
                 <span uk-icon="icon: cloud-upload"></span>
                 <span class="uk-text-middle">Sleep hier een foto in of </span>
                 <div uk-form-custom>
-                    <input type="file" multiple>
+                    <input type="file" name="Image" multiple>
                     <span class="uk-link">selecteer er een</span>
                 </div>
             </div>
@@ -102,83 +103,82 @@ echo '
             <progress id="js-progressbar" class="uk-progress" value="0" max="100" hidden></progress>
 
 ';
-?>
+    ?>
 
-            <script>
+    <script>
 
-                var bar = document.getElementById('js-progressbar');
+        var bar = document.getElementById('js-progressbar');
 
-                UIkit.upload('.js-upload', {
+        UIkit.upload('.js-upload', {
 
-                    url: 'images',
-                    multiple: true,
+            url: 'images',
+            multiple: true,
 
-                    beforeSend: function () {
-                        console.log('beforeSend', arguments);
-                    },
-                    beforeAll: function () {
-                        console.log('beforeAll', arguments);
-                    },
-                    load: function () {
-                        console.log('load', arguments);
-                    },
-                    error: function () {
-                        console.log('error', arguments);
-                    },
-                    complete: function () {
-                        console.log('complete', arguments);
-                    },
+            beforeSend: function () {
+                console.log('beforeSend', arguments);
+            },
+            beforeAll: function () {
+                console.log('beforeAll', arguments);
+            },
+            load: function () {
+                console.log('load', arguments);
+            },
+            error: function () {
+                console.log('error', arguments);
+            },
+            complete: function () {
+                console.log('complete', arguments);
+            },
 
-                    loadStart: function (e) {
-                        console.log('loadStart', arguments);
+            loadStart: function (e) {
+                console.log('loadStart', arguments);
 
-                        bar.removeAttribute('hidden');
-                        bar.max = e.total;
-                        bar.value = e.loaded;
-                    },
+                bar.removeAttribute('hidden');
+                bar.max = e.total;
+                bar.value = e.loaded;
+            },
 
-                    progress: function (e) {
-                        console.log('progress', arguments);
+            progress: function (e) {
+                console.log('progress', arguments);
 
-                        bar.max = e.total;
-                        bar.value = e.loaded;
-                    },
+                bar.max = e.total;
+                bar.value = e.loaded;
+            },
 
-                    loadEnd: function (e) {
-                        console.log('loadEnd', arguments);
+            loadEnd: function (e) {
+                console.log('loadEnd', arguments);
 
-                        bar.max = e.total;
-                        bar.value = e.loaded;
-                    },
+                bar.max = e.total;
+                bar.value = e.loaded;
+            },
 
-                    completeAll: function () {
-                        console.log('completeAll', arguments);
+            completeAll: function () {
+                console.log('completeAll', arguments);
 
-                        setTimeout(function () {
-                            bar.setAttribute('hidden', 'hidden');
-                        }, 1000);
+                setTimeout(function () {
+                    bar.setAttribute('hidden', 'hidden');
+                }, 1000);
 
-                        alert('Upload Completed');
-                    }
+                alert('Upload Completed');
+            }
 
-                });
+        });
 
-            </script>
-        </div>
+    </script>
 
-        <div class="uk-card-refactor auctions-reset-margin uk-display-inline-block uk-margin-top-zero">
-            <form class="uk-form-horizontal uk-margin-large">
-                <div class="uk-margin">
-                    <label class="uk-form-label" for="form-horizontal-select">Beschrijving</label>
-                    <textarea class="uk-textarea" rows="5" placeholder="Textarea"></textarea>
-                </div>
-            </form>
+    <div class="auctions-reset-margin uk-display-inline-block uk-margin-top-zero">
+        <div class="uk-margin">
+            <label class="uk-form-label" for="form-horizontal-select">Beschrijving</label>
+            <textarea class="uk-textarea" rows="5" placeholder="Textarea" name="Beschrijving"></textarea>
         </div>
     </div>
+    </div>
 
-        <div class="uk-card-refactor2 auctions-reset-margin uk-display-inline-block uk-margin-top-zero">
-            <a class="uk-button uk-button-primary uk-button-reset" href="#">Plaats Advertentie</a>
-        </div>
+    <div class="uk-card-refactor auctions-reset-margin uk-display-inline-block uk-margin-top-zero">
+        <input class="uk-button uk-button-primary uk-button-reset" type="submit" name="submit"
+               value="Plaats advertentie">
+    </div>
+    </form>
 
     <?php
 
