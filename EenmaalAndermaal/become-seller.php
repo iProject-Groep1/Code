14800442be45c44ea3a3fcd967f99e9f93618c31
synolog1x <sanderrdtc@gmail.com>
2018-06-05 11:15:46 +0200

    check voor login

--- EenmaalAndermaal/become-seller.php
+++ EenmaalAndermaal/become-seller.php
@@ -4,6 +4,22 @@ $pageTitle = "Verkoper worden";
 require_once('scripts/header.php');
 require_once('scripts/become-seller-functions.php');
 require_once('scripts/database-connect.php');
+
+if(!isset($_SESSION)){
+    session_start();
+}
+
+if (!isset($_SESSION['username'])) {
+    //TODO: melding werkend maken
+    $_SESSION['logMelding'] = '<script style="border-radius: 25px;">UIkit.notification({message: \'<span uk-icon="icon: sign-in"></span> U moet inloggen om deze pagina te bezoeken.\', status: \'danger\'})</script>';
+    header('Location: login.php');
+}
+
+if (isset($_SESSION['becomeSellerFormNotification']) && !empty($_SESSION['becomeSellerFormNotification'])) {
+    echo $_SESSION['becomeSellerFormNotification'];
+    $_SESSION['becomeSellerFormNotification'] = "";
+}
+
 ?>
 
     <h2 class="uk-text-center">Verkoper worden</h2>
@@ -16,9 +32,17 @@ require_once('scripts/database-connect.php');
             <!-- TODO: maxlengths -->
             <div class="uk-margin uk-form-horizontal">
                 <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="paymentMethod">Verificatiemethode
-                    :</label>
+                    : <span uk-icon="question"></span>
+                    <div uk-drop>
+                        <div class="uk-card uk-card-body uk-card-default"><p class="uk-text-primary">Wanneer u kiest
+                                voor post krijgt u een brief thuisgestuurd met een verificatiecode die u moet invullen.
+                                Wanneer u voor creditcard kiest nemen wij contact op met de creditcardmaatschappij en
+                                krijgt u een mail wanneer uw gegevens geverifiëerd zijn.</p></div>
+                    </div>
+                </label>
                 <div class="uk-margin uk-grid-small uk-child-width-1-3 uk-grid">
-                    <label><input class="uk-radio" type="radio" name="verificationMethod" value="Post" checked> Post</label>
+                    <label><input class="uk-radio" type="radio" name="verificationMethod" value="Post" checked>
+                        Post</label>
                     <label><input class="uk-radio" type="radio" name="verificationMethod" value="Creditcard"> Creditcard</label>
                 </div>
             </div>
@@ -27,9 +51,9 @@ require_once('scripts/database-connect.php');
                     :</label>
                 <div class="uk-inline uk-width-2-3">
                     <span class="uk-form-icon" uk-icon="icon: credit-card"></span>
-                    <input class="uk-input" type="text" placeholder="Creditcardnummer" name="creditCardNumber"
+                    <input class="uk-input" type="number" placeholder="Creditcardnummer" name="creditCardNumber"
                            id="creditCardNumber"
-                           maxlength="20" required>
+                           maxlength="16" required>
                 </div>
             </div>
             <div class="uk-margin uk-form-horizontal">
@@ -43,18 +67,18 @@ require_once('scripts/database-connect.php');
                     :</label>
                 <div class="uk-inline uk-width-2-3">
                     <span class="uk-form-icon" uk-icon="icon: credit-card"></span>
-                    <input class="uk-input" type="text" placeholder="Rekeningnummer" name="bankAccountNumber"
+                    <input class="uk-input" type="number" placeholder="Rekeningnummer" name="bankAccountNumber"
                            id="bankAccountNumber"
-                           maxlength="20" required>
+                           maxlength="34" required>
                 </div>
             </div>
             <div class="uk-margin uk-form-horizontal">
-                <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="password">Creditcardnummer
+                <label class="uk-form-label uk-width-1-3 uk-margin-small-bottom" for="password">Wachtwoord
                     :</label>
                 <div class="uk-inline uk-width-2-3">
                     <span class="uk-form-icon" uk-icon="icon: lock"></span>
                     <input class="uk-input" type="text" placeholder="Wachtwoord" name="password" id="password"
-                           maxlength="20" required>
+                           maxlength="72" required>
                 </div>
             </div>
             <div class="uk-margin uk-flex uk-flex-center ">
