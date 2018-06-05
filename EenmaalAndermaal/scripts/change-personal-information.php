9c0eaa00313a00bffc7e1c5dd89feb94cf4c0690
JasmijnB97 <jasmijn.bartelds@hotmail.nl>
2018-06-05 09:49:39 +0200

    Fout gefixt

--- EenmaalAndermaal/scripts/change-personal-information.php
+++ EenmaalAndermaal/scripts/change-personal-information.php
@@ -10,19 +10,22 @@ $postalCode = $_POST['postalCode'];
 $placeName = $_POST['placeName'];
 $country = $_POST['country'];
 
-//empty(trim($postalCode)) || empty(trim($placeName)) || empty(trim($country))
 if (empty(trim($firstname)) || empty(trim($lastname)) || empty(trim($birthday)) || empty(trim($adres1)) || empty(trim($postalCode)) || empty(trim($placeName)) || empty(trim($country))) {
     header('Location: ../profile.php?');
     $_SESSION['noChance'] = '
     <script style="border-radius: 25px;">UIkit.notification({message: \'<span uk-icon="icon: sign-in"></span> Er zijn geen gegevens veranderd.\', status: \'danger\'})</script>';
 } else {
-    //postcode = '$postalCode', plaatsnaam = '$placeName', land = '$country'
-    $sql = "UPDATE Gebruiker SET voornaam = '$firstname', achternaam= '$lastname', geboortedag = '$birthday', adresregel1 = '$adres1', postcode = '$postalCode', plaatsnaam = '$placeName', land = '$country' WHERE gebruikersnaam = '$username'";
-    $stmt = $dbh->prepare($sql);
-    $stmt->execute();
-    header('Location: ../profile.php?');
-    $_SESSION['chance'] = '
-    <script style="border-radius: 25px;">UIkit.notification({message: \'<span uk-icon="icon: sign-in"></span> Uw gegevens zijn veranderd.\', status: \'success\'})</script>';
+    try{
+        $sql = "UPDATE Gebruiker SET voornaam = '$firstname', achternaam= '$lastname', geboortedag = '$birthday', adresregel1 = '$adres1', postcode = '$postalCode', plaatsnaam = '$placeName', land = '$country' WHERE gebruikersnaam = '$username'";
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute();
+        header('Location: ../profile.php?');
+        $_SESSION['chance'] = '
+        <script style="border-radius: 25px;">UIkit.notification({message: \'<span uk-icon="icon: sign-in"></span> Uw gegevens zijn veranderd.\', status: \'success\'})</script>';
+    } catch (PDOException $e) {
+        echo "Fout" . $e->getMessage();
+        header('Location: errorpage.php?err=500');
+    }
 }
 
 
