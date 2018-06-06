678ebefc9bb49dab31c9a82a28c648c99395405a
Daan919 <38421133+Daan919@users.noreply.github.com>
2018-06-06 12:41:48 +0200

    tada

--- EenmaalAndermaal/SearchResultsRefine.php
+++ EenmaalAndermaal/SearchResultsRefine.php
@@ -5,12 +5,30 @@ include('scripts/search.php');
 include('scripts/database-connect.php');
 
 if(isset($_POST['submit'])){
-  getVerfijn($dbh);
+?>  <div class="uk-grid">
+   <div class="uk-width-3-4"style="display:inline; margin-left: 20px;">
+      <div class="uk-card auctions-reset-margin uk-card-default no-shadow uk-card-body">
+  <h3 class="uk-display-block uk-align-left uk-text-center">Zoekresultaten  </h3>
+  <p>
+  <div class="uk-grid uk-align-left uk-width-medium-1-4 uk-flex uk-flex-left auctions-reset-margin">
+    <?php $echo = getVerfijn($dbh); echo $echo;?>
+  </div>
+  </p>
+  </div>
+  </div>
+  <div class="uk-align-right uk-width-1-5 uk-card auctions-reset-margin uk-card-default no-shadow uk-card-body"  >
+  <form class="uk-search uk-search-default uk-align-right "action="../SearchResults.php" method="post">
+    <label for="searchterm">Nieuwe zoekterm</label>
+    <input class="uk-search-input" type="search" name="Searching" placeholder="Search..." >
+  </form>
+</div>
+
+
+</div>
+
+
+<?php
 }
-
-
-
-
 
 include('scripts/footer.php');
 ?>
--- EenmaalAndermaal/scripts/search.php
+++ EenmaalAndermaal/scripts/search.php
@@ -39,7 +39,6 @@ function getSearchItems($dbh, $query,$bindValue)
         }
     } catch (PDOException $e) {
         echo "Fout" . $e->getMessage();
-        die();
         header('Location: ../errorpage.php?err=500');
     }
     return $itemCards;
@@ -60,12 +59,12 @@ try {
   $stmt->bindValue(":bindValue", $bindValue , PDO::PARAM_STR); /* helpt tegen SQL injection */
   $stmt->execute(); /* stuurt alles naar de server */
 
-$X=0;
+
   while ($row = $stmt->fetch()) {
 /* <input type="checkbox" name="vehicle1" value="Bike"> I have a bike<br>*/
 
-        $return .= '<input type="checkbox" name="rubriek['.$X.']" value="'. $row['rubrieknaam'] . '"> '. $row['rubrieknaam'] . ' (' . $row['aantal'] .')<br>';
-          $X=$X+1;
+        $return .= '<input type="radio" name="rubriek" value="'. $row['rubrieknaam'] . '"> '. $row['rubrieknaam'] . ' (' . $row['aantal'] .')<br>';
+
             }
       echo $return;
   }
@@ -77,29 +76,38 @@ $X=0;
 }
 
 function getVerfijn($dbh){
-$rubrieken = array();
-$rubrieken =  $_POST['rubriek'];
-sort($rubrieken);
-$bindValue = '%boot%';
 
-$or = '';
- foreach ($rubrieken as $key ) {
-   $or .= " rubrieknaam =  $key  or ";
- }
- $or = substr($or, 0 , -3);
 
-$query=  'SELECT  v.voorwerpnummer, v.titel, v.looptijdEindmoment, (SELECT TOP 1 filenaam FROM bestand f WHERE v.voorwerpnummer = f.voorwerp)
+$bindValue = '%'. $_POST['searchterm'] .'%' ;
+$bindValue2 =$_POST['rubriek'];
+
+$query=  "SELECT  v.voorwerpnummer, v.titel, v.looptijdEindmoment, (SELECT TOP 1 filenaam FROM bestand f WHERE v.voorwerpnummer = f.voorwerp)
           AS bestandsnaam, MAX(Bodbedrag) AS hoogsteBod, CURRENT_TIMESTAMP AS serverTijd,startprijs
          FROM Voorwerp v left join Bod b ON v.voorwerpnummer = b.voorwerp
                          join VoorwerpInRubriek r ON v.voorwerpnummer = r.voorwerp
-         WHERE titel like :bindValue and veilinggesloten = 0 and voorwerpnummer in
-          (select voorwerp from VoorwerpInRubriek vr join Rubriek r on r.rubrieknummer = vr.rubriek_op_laagste_Niveau where
-               '.$or.') GROUP BY Voorwerpnummer, titel, looptijdEindmoment,startprijs' ; /* prepared statement */
+         WHERE titel like ? and veilinggesloten = 0 and voorwerpnummer in
+         (select voorwerp from VoorwerpInRubriek vr join Rubriek r on r.rubrieknummer = vr.rubriek_op_laagste_Niveau where rubrieknaam = ? )
+           GROUP BY Voorwerpnummer, titel, looptijdEindmoment,startprijs" ; /* prepared statement */
 
-die();
-echo getSearchItems($dbh, $query,$bindValue);
+
+           $itemCards = "";
+           try {
+               $stmt = $dbh->prepare($query); /* prepared statement */
+               $stmt->bindValue(1, $bindValue , PDO::PARAM_STR); /* helpt tegen SQL injection */
+               $stmt->bindValue(2, $bindValue2 , PDO::PARAM_STR); /* helpt tegen SQL injection */
+               $stmt->execute(); /* stuurt alles naar de server */
+               while ($results = $stmt->fetch()) {
+
+                   $price = $results['hoogsteBod'];
+                   if(is_null($price)){
+                       $price =$results['startprijs']  ;
+                   }
+                   $itemCards .= createItemScript($results['titel'], $results['looptijdEindmoment'], $results['bestandsnaam'], $price, $results['voorwerpnummer'], $dbh);
+               }
+           } catch (PDOException $e) {
+               echo "Fout" . $e->getMessage();
+               header('Location: ../errorpage.php?err=500');
+           }
+        return $itemCards;
 }
-
-
-
  ?>
